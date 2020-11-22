87ededc38963ed18adcdd8b7869e48832cc316dd
Keith Bauson <kwbauson@gmail.com>
2020-11-22 14:17:29 -0600

    add server public key

--- home.nix
+++ home.nix
@@ -438,6 +438,7 @@ with builtins; with pkgs.mylib; {
     target = ".ssh/authorized_keys";
     text = ''
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQ14QkMDvnwU60jit5M8x/KVrUkjxbkVyDdRY8IbPsB keith@keith-xps
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJtcd3xL1BR1RSFzin0Im+HUk6kTWj44wJ56uWxkPM0 keith@kwbauson
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaLZgemYIGQD0yvRU38tvYR4Hp4nLAdZMj9jgwS+p0y keith@keith-vm
     '';
   };
