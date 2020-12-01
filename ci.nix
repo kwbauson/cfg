776ca194cb46b8c2cd199c83d53c3758baebcc26
Keith Bauson <kwbauson@gmail.com>
2020-12-01 14:02:51 -0500

    sads hercules not work

--- ci.nix
+++
@@ -1,9 +0,0 @@
-with import ./flake-compat.nix;
-{
-  nixosConfigurations-keith-xps = nixosConfigurations.keith-xps.drv;
-  nixosConfigurations-kwbauson = nixosConfigurations.kwbauson.drv;
-  nixosConfigurations-keith-vm = nixosConfigurations.keith-vm.drv;
-
-  # homeConfigurations-non-graphical = homeConfigurations.non-graphical;
-  # homeConfigurations-graphical = homeConfigurations.graphical;
-}
