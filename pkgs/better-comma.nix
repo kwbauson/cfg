31c150dfde6f94384cea0a09e597fc607d0409b3
Keith Bauson <kwbauson@gmail.com>
2021-01-15 11:24:34 -0500

    add better-comma as command

--- pkgs/better-comma.nix
+++ pkgs/better-comma.nix
@@ -1,70 +1,74 @@
 pkgs: with pkgs; with mylib; buildEnv rec {
   inherit name;
-  paths = [
-    (
+  paths =
+    let
+      better-comma-script =
+        (
+          writeShellScriptBin ","
+            ''
+              ${pathAdd [ sqlite coreutils fzy nix-wrapped ]}
+              set -e
+              [[ $1 = -u ]] && uncache=1 && shift
+              [[ $1 = -d ]] && desc=1 && shift
+              cmd=$1
+              if [[ -z $cmd || $cmd = -h || $cmd = --help ]];then
+                echo usage: , COMMAND ARGS
+                exit
+              fi
+              sql="select distinct package from Programs where name = '$cmd'"
+              packages=$(sqlite3 -init /dev/null ${programs-sqlite} "$sql" 2> /dev/null)
+
+              if [[ $(echo "$packages" | wc -l) = 1 ]];then
+                if [[ -z $packages ]];then
+                  attr=$cmd
+                else
+                  attr=$packages
+                fi
+              else
+                cachefile=~/.cache/${name}/$cmd
+                [[ -n $uncache ]] && rm -f "$cachefile"
+                if [[ -e $cachefile ]];then
+                  attr=$(< "$cachefile")
+                else
+                  attr=$(echo "$packages" | fzy)
+                  mkdir -p "$(dirname "$cachefile")"
+                  echo "$attr" > "$cachefile"
+                fi
+              fi
+              if [[ -n $attr ]];then
+                if [[ $desc = 1 ]];then
+                  exec nix eval --impure --expr "with import ${selfpkgs.outPath}; desc $attr"
+                else
+                  exec nix shell "${selfpkgs.outPath}#$attr" --command "$@"
+                fi
+              fi
+            ''
+        ).overrideAttrs (_: { inherit name; });
+    in
+    [
+      better-comma-script
+      (alias "better-comma" "${better-comma-script}/bin/,")
       (
-        writeShellScriptBin ","
+        writeTextDir "etc/bash_completion.d/${name}.sh"
           ''
-            ${pathAdd [ sqlite coreutils fzy nix-wrapped ]}
-            set -e
-            [[ $1 = -u ]] && uncache=1 && shift
-            [[ $1 = -d ]] && desc=1 && shift
-            cmd=$1
-            if [[ -z $cmd || $cmd = -h || $cmd = --help ]];then
-              echo usage: , COMMAND ARGS
-              exit
-            fi
-            sql="select distinct package from Programs where name = '$cmd'"
-            packages=$(sqlite3 -init /dev/null ${programs-sqlite} "$sql" 2> /dev/null)
+            _${name}()
+            {
+                ${pathAdd [ sqlite ]}
+                local cur prev opts sql
+                COMPREPLY=()
+                cur="''${COMP_WORDS[COMP_CWORD]}"
+                prev="''${COMP_WORDS[COMP_CWORD-1]}"
+                sql="select distinct name from Programs where name like '$cur%' order by name"
 
-            if [[ $(echo "$packages" | wc -l) = 1 ]];then
-              if [[ -z $packages ]];then
-                attr=$cmd
-              else
-                attr=$packages
-              fi
-            else
-              cachefile=~/.cache/${name}/$cmd
-              [[ -n $uncache ]] && rm -f "$cachefile"
-              if [[ -e $cachefile ]];then
-                attr=$(< "$cachefile")
-              else
-                attr=$(echo "$packages" | fzy)
-                mkdir -p "$(dirname "$cachefile")"
-                echo "$attr" > "$cachefile"
-              fi
-            fi
-            if [[ -n $attr ]];then
-              if [[ $desc = 1 ]];then
-                exec nix eval --impure --expr "with import ${selfpkgs.outPath}; desc $attr"
-              else
-                exec nix shell "${selfpkgs.outPath}#$attr" --command "$@"
-              fi
-            fi
+                if [[ $COMP_CWORD = 1 ]] || [[ $prev = -* && $COMP_CWORD = 2 ]];then
+                  COMPREPLY=( $(compgen -W "$(sqlite3 -init /dev/null ${programs-sqlite} "$sql" 2> /dev/null)" -- "$cur") )
+                else
+                  COMPREPLY=( $(compgen -f -- "$cur") )
+                fi
+                return 0
+            }
+            complete -F _${name} ,
           ''
-      ).overrideAttrs (_: { inherit name; })
-    )
-    (
-      writeTextDir "etc/bash_completion.d/${name}.sh"
-        ''
-          _${name}()
-          {
-              ${pathAdd [ sqlite ]}
-              local cur prev opts sql
-              COMPREPLY=()
-              cur="''${COMP_WORDS[COMP_CWORD]}"
-              prev="''${COMP_WORDS[COMP_CWORD-1]}"
-              sql="select distinct name from Programs where name like '$cur%' order by name"
-
-              if [[ $COMP_CWORD = 1 ]] || [[ $prev = -* && $COMP_CWORD = 2 ]];then
-                COMPREPLY=( $(compgen -W "$(sqlite3 -init /dev/null ${programs-sqlite} "$sql" 2> /dev/null)" -- "$cur") )
-              else
-                COMPREPLY=( $(compgen -f -- "$cur") )
-              fi
-              return 0
-          }
-          complete -F _${name} ,
-        ''
-    )
-  ];
+      )
+    ];
 }
