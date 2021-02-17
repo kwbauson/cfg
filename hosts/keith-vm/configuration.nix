83c7259cdde1204c64bf0817acac780a651f04f7
Keith Bauson <kwbauson@gmail.com>
2021-02-17 15:58:23 -0500

    some mimir classroom nginx setup

--- hosts/keith-vm/configuration.nix
+++ hosts/keith-vm/configuration.nix
@@ -13,4 +13,16 @@
   programs.steam.enable = true;
   services.gnome3.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
+
+  services.sshd.enable = true;
+  services.nginx = {
+    enable = true;
+    virtualHosts.localhost = {
+      listen = [{ addr = "127.0.0.1"; port = 8000; }];
+      locations = {
+        "/".proxyPass = "http://127.0.0.1:7999";
+        "~ ^/(?:api|internal|lms|launch|users/lti_login)(/.*|$)".proxyPass = "http://127.0.0.1:3000";
+      };
+    };
+  };
 }
