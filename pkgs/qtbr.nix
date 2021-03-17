4ca7ab32718050188d3ce6677a909c60c8bdcb9d
Keith Bauson <kwbauson@gmail.com>
2021-03-17 14:03:15 -0400

    drop dmg-qutebrowser

--- pkgs/qtbr.nix
+++ pkgs/qtbr.nix
@@ -1,11 +1,5 @@
-pkgs: with pkgs; with mylib;
-let root =
-  if isDarwin
-  then unpack qutebrowser.originalPackage.src
-  else "${qutebrowser}/share/qutebrowser";
-in
-runBin name ''
-  cat ${root}/scripts/open_url_in_instance.sh |
+pkgs: with pkgs; with mylib; runBin name ''
+  cat ${qutebrowser}/share/qutebrowser/scripts/open_url_in_instance.sh |
     sed 's/_url="$1"/_url="$\{1-about:blank}"/' |
     ${optionalString isDarwin "sed 's:_ipc_socket=.*:_ipc_socket=/tmp/qutebrowser-socket:' |"}
     sed 's/"$_qute_bin"/exec "$_qute_bin"/' |
