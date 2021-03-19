8cbd70faa3d0427f2ae82799198c658dc85a2bbf
Keith Bauson <kwbauson@gmail.com>
2021-03-19 13:13:52 -0400

    nixconfig fallback true

--- flake.nix
+++ flake.nix
@@ -103,6 +103,7 @@ rec {
         narinfo-cache-negative-ttl = 5
         connect-timeout = 5
         download-attempts = 2
+        fallback = true
       '';
 
       inherit (self.packages.x86_64-linux) programs-sqlite;
