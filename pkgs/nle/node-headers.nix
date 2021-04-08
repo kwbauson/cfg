fd2934fbc30a3f7be83b4d8ff5372aadc200223e
kwbauson <kwbauson@users.noreply.github.com>
2021-04-08 15:52:01 +0000

    Automatic update

--- pkgs/nle/node-headers.nix
+++ pkgs/nle/node-headers.nix
@@ -1,1 +1,1 @@
-builtins.fetchurl { url = "https://nodejs.org/download/release/v15.13.0/node-v15.13.0-headers.tar.gz"; sha256 = "1kcb6zqif9cnixqmyx65lj2x8n1ic9cnmkb8a8pd8ngyhmybgi2c"; }
+builtins.fetchurl { url = "https://nodejs.org/download/release/v15.14.0/node-v15.14.0-headers.tar.gz"; sha256 = "0chi2ay6yhzkwljfkra96w3p1r6wd2ysxzznkpq93jv1rciiqngs"; }
