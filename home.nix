37de39b14bcf517ff5a1a79aa1c184e3588a4c70
Keith Bauson <kwbauson@gmail.com>
2021-04-15 18:27:51 -0400

    dark theme for qt

--- home.nix
+++ home.nix
@@ -512,6 +512,7 @@ with builtins; with pkgs; with mylib; {
   '';
 
   gtk.enable = isLinux;
+  gtk.theme.name = "Adwaita-dark";
   gtk.gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
   qt.enable = isLinux;
   qt.platformTheme = "gtk";
