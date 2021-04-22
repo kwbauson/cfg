2d3268650c1e522645190a77a055e82f38dd3da5
Keith Bauson <kwbauson@gmail.com>
2021-04-22 11:52:38 -0400

    i do not understand

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 let
   getFlake = src:
-    if builtins ? getFlake
-    then builtins.getFlake (toString src)
-    else (import (import ./nix/sources.nix).flake-compat { src = ./.; }).defaultNix;
+    (import (import ./nix/sources.nix).flake-compat { src = ./.; }).defaultNix;
 in
 (getFlake ./.).packages.${builtins.currentSystem}
