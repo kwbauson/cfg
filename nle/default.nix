f550ea26b8396ffac6206fb5fe0ffedcffa4c5ac
Keith Bauson <kwbauson@gmail.com>
2021-04-22 20:49:16 -0400

    memes with new nle

---
+++ nle/default.nix
@@ -0,0 +1,1 @@
+import ./eval-config.nix ./nle-config.nix
---
+++ nle/nle-config.nix
@@ -0,0 +1,37 @@
+config: with config.lib; {
+  lib = builtins // {
+    inherit (config.nixpkgs.pkgs.lib) flatten;
+    file = p: config.source + "/${p}";
+    hasFile = p: tryFile p != null;
+    tryFile = p:
+      let f = file p; in
+      if config ? source && pathExists f then f else null;
+    tryRead = p: if tryFile p == null then "" else readFile (tryFile p);
+    tryImport = p: nul:
+      let f = tryFile p; in if f == null then nul else import f;
+    makePaths = attrs:
+      if attrs.enable or true
+      then if attrs ? paths then attrs else if attrs ? build then attrs // {
+        paths = attrs.build attrs.settings or { };
+      } else attrs else attrs // { paths = [ ]; };
+  };
+  nixpkgs = {
+    config = tryImport "config.nix" { };
+    overlays = tryImport "overlays.nix" [ ];
+    pkgs = import config.nixpkgs.path {
+      inherit (config.nixpkgs) system config overlays;
+    };
+  };
+  bundler = makePaths {
+    enable = all hasFile [ "Gemfile" "Gemfile.lock" "gemset.nix" ];
+    build = config.nixpkgs.pkgs.bundlerEnv;
+    settings = {
+      name = "bundler-env";
+      gemfile = tryFile "Gemfile";
+      lockfile = tryFile "Gemfile.lock";
+      gemset = tryFile "gemset.nix";
+    };
+  };
+
+  paths._replace = flatten (map (x: x.paths or [ ]) (attrValues (config // { paths = null; })));
+}
