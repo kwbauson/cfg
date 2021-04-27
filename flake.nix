792d6a8810dd6e5d2bb13a51f5b29a7989fa4466
Keith Bauson <kwbauson@gmail.com>
2021-04-27 10:07:23 -0400

    Revert "enable ca-derivations"
    This reverts commit 6239fa98af9270634cfde32633e977a93b0250f0.

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@ rec {
         max-jobs = auto
         keep-going = true
         builders-use-substitutes = true
-        extra-experimental-features = nix-command flakes ca-references ca-derivations
+        extra-experimental-features = nix-command flakes ca-references
         substituters = ${toString nixConfig.substituters}
         extra-trusted-public-keys = ${toString nixConfig.extra-trusted-public-keys}
         keep-env-derivations = true
