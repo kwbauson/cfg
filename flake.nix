19c087490d67d8c76d0e9254a287d339c005bdae
Keith Bauson <kwbauson@gmail.com>
2021-04-30 08:18:10 -0400

    emem

--- flake.nix
+++ flake.nix
@@ -169,7 +169,7 @@ rec {
 
       inherit (self.packages.x86_64-linux) self-source;
 
-      outputs = { inherit keith-xps keith-desktop kwbauson keith-vm; };
+      outputs = { inherit self-source keith-xps keith-desktop kwbauson keith-vm; };
       output-paths = generators.toKeyValue { } (mapAttrs (n: v: toString v) outputs);
 
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.linkFarmFromDrvs "build" (attrValues outputs);
