cb51fd82fb03bcd282455d06ab03f8b7af8b577c
Keith Bauson <kwbauson@gmail.com>
2021-04-30 08:16:50 -0400

    blarg

--- home.nix
+++ home.nix
@@ -90,9 +90,10 @@ with builtins; with pkgs; with mylib; {
             hmg
             cd ~/cfg
             git a
+            source_path=$(nix eval --raw .#self-source --no-warn-dirty)
             host=$(built-as-host)
             path=$(sed -n "s/$host=//p" output-paths)
-            if [[ -n $path && $(nix eval --raw .#self-source --no-warn-dirty) = $(< source-path) ]];then
+            if [[ -n $path ]] && grep -qF "$source_path" output-paths;then
               if nix build --no-link "$path";then
                 exec "$path"/bin/switch
               else
