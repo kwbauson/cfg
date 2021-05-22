773b96fff75488eadf9f187b79e264bd3a38395f
Keith Bauson <kwbauson@gmail.com>
2021-05-22 11:00:13 -0400

    fix getFlake

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
 let
   flake-compat = import (import ./nix/sources.nix).flake-compat;
-  getFlake = builtins.getFlake or (src:
-    (flake-compat { inherit src; }).defaultNix
-  );
-  cfg = getFlake (builtins.path { name = "source"; path = ./.; });
+  getFlake = path:
+    if builtins ? getFlake
+    then builtins.getFlake (toString path)
+    else (flake-compat { src = builtins.path { name = "source"; inherit path; }; }).defaultNix;
+  cfg = getFlake ./.;
 in
 cfg.packages.${builtins.currentSystem} // { inherit getFlake cfg; }
