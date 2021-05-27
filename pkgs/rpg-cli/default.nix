2060aa9c7cbe99fb924d91286d7fd7463a9763e3
Keith Bauson <kwbauson@gmail.com>
2021-05-26 23:27:57 -0400

    remove rpg-cli

--- pkgs/rpg-cli/default.nix
+++
@@ -1,6 +0,0 @@
-pkgs: with pkgs; rustPlatform.buildRustPackage {
-  inherit name src;
-  cargoPatches = [ ./Cargo.lock.patch ];
-  cargoHash = "sha256-Y2ruzA3HNNBJq0j2SygRqL0JKoNI9tWejZfrLSsIt6c=";
-  doCheck = false;
-}
