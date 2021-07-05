43d04bfd7a80a9311d31c5dc409a2d818dddb74a
Keith Bauson <kwbauson@gmail.com>
2021-07-05 19:30:13 -0400

    oops include checks in build

--- flake.nix
+++ flake.nix
@@ -167,8 +167,9 @@ rec {
         imports = [ nixosModules.installer.cd-dvd.installation-cd-graphical-gnome ];
       }).config.system.build.isoImage;
 
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.linkFarmFromDrvs "build" (attrValues outputs);
-      defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.linkFarmFromDrvs "build" [ checks-mac keith-mac ];
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.linkFarmFromDrvs "build"
+        (attrValues outputs ++ [ checks ]);
+      defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.linkFarmFromDrvs "build"
+        [ checks-mac keith-mac ];
     };
 }
-
