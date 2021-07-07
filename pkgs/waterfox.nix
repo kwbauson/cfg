ceed1c60374aecd8efbcca75220704b8426a74c8
Keith Bauson <kwbauson@gmail.com>
2021-07-07 10:35:43 -0400

    add waterfox

---
+++ pkgs/waterfox.nix
@@ -0,0 +1,11 @@
+pkgs: with pkgs; with mylib; stdenv.mkDerivation {
+  inherit name src;
+  nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs = firefox-unwrapped.buildInputs;
+  installPhase = ''
+    mkdir -p $out/{share,bin}
+    cp -r . $out/share/waterfox
+    ln -s $out/share/waterfox/waterfox $out/bin
+  '';
+}
+
