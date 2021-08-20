25e505561ebbba971566395a0a5e228c25d888fb
Keith Bauson <kwbauson@gmail.com>
2021-08-20 01:31:59 +0000

    make jitsi script use patch

--- scripts/build-jitsi.sh
+++ scripts/build-jitsi.sh
@@ -2,7 +2,7 @@
 set -ex
 git rt
 git clean -fdx
-git stash apply
+git apply "$(dirname "$(realpath "$0")")"/jitsi.patch
 npm_config_package_lock_only= nix shell ~/cfg#{nodejs_latest,gnumake,python2} -c npm i
 nix shell ~/cfg#{nodejs_latest,gnumake,python2} -c make compile deploy source-package
 scp jitsi-meet.tar.bz2 kwbauson.com:cfg
