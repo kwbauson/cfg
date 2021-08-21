0287f8e2913f0daed088bf272f811a12d07a3e17
Keith Bauson <kwbauson@gmail.com>
2021-08-21 02:18:40 +0000

    don't remove node_modules in jitsi build

--- scripts/build-jitsi.sh
+++ scripts/build-jitsi.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 git rt
-git clean -fdx
 git apply "$(dirname "$(realpath "$0")")"/jitsi.patch
 npm_config_package_lock_only= nix shell ~/cfg#{nodejs_latest,gnumake,python2} -c npm i
 nix shell ~/cfg#{nodejs_latest,gnumake,python2} -c make compile deploy source-package
