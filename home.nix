a5ddf59301cfeff70fd519e1a0c8a66222a2057a
Keith Bauson <kwbauson@gmail.com>
2021-08-26 10:32:11 -0400

    plz fail

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@ with builtins; with pkgs; with mylib; {
             which xdg_utils xxd xz zip bitwarden-cli libqalculate youtube-dl
             speedtest-cli tldr nix-top nixos-install-tools
             ;
+          better-comma = better-comma.unwrapped;
         };
         ${attrIf isGraphical "graphical"} = {
           graphical-core = {
