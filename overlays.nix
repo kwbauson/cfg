9d4924c2100e57a417669c49e9b4d3142a503a7a
Keith Bauson <kwbauson@gmail.com>
2021-08-26 09:01:21 -0400

    Revert "attempt to fix failing hercules evaluation"
    This reverts commit 7b0af4b8cea3c78c395b94d2e65dcf450c9b074d.

--- overlays.nix
+++ overlays.nix
@@ -28,7 +28,7 @@
     programs-sqlite = stdenv.mkDerivation rec {
       name = "programs-sqlite";
       buildInputs = [ sqlite ];
-      src = fetchTarball { inherit (sources.nixos-unstable) url sha256; };
+      dontUnpack = true;
       extraPrograms =
         let
           subPackages = name: map (x: "${name}.${x}") (attrNames self.${name});
@@ -49,7 +49,7 @@
           );
       passAsFile = "extraPrograms";
       installPhase = ''
-        cp $src/programs.sqlite $out
+        cp ${sources.nixos-unstable}/programs.sqlite $out
         sqlite3 $out --csv 'select * from Programs' > current
         grep -vFf current $extraProgramsPath > extra
         chmod +w $out
