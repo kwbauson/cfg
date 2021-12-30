8d5ee3c2ef06a51ad71e90711f82df75787ad08f
Keith Bauson <kwbauson@gmail.com>
2021-12-30 16:56:31 -0500

    back to using flake builtin if it exists

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
 let
   flake-compat = import (import ./nix/sources.nix).flake-compat;
-  getFlake = path: (flake-compat { src = builtins.path { name = "source"; inherit path; }; }).defaultNix;
+  getFlake = path:
+    if builtins ? getFlake
+    then builtins.getFlake (toString path)
+    else (flake-compat { src = builtins.path { name = "source"; inherit path; }; }).defaultNix;
   cfg = getFlake ./.;
   pkgs = cfg.packages.${builtins.currentSystem};
 in
-pkgs.mylib // cfg.outputs // pkgs
+{ inherit getFlake; } // pkgs.mylib // cfg.outputs // pkgs
