efcd439fd9e84a1c5a53a2a77c2db0fe60d7a59a
Keith Bauson <kwbauson@gmail.com>
2022-02-14 19:48:39 -0500

    it lives!

--- pkgs/jitsi-meet/default.nix
+++ pkgs/jitsi-meet/default.nix
@@ -4,26 +4,26 @@ let
     tar xvf ${nodejs.src}
     mv node-* $out
   '';
-  yarnModules = yarn2nix-moretea.mkYarnModules {
-    pname = "jitsi-meet-modules";
-    inherit version;
-    packageJSON = "${sources.jitsi-meet}/package.json";
-    yarnLock = ./yarn.lock;
-    yarnNix = ./yarn.nix;
-    preBuild = ''
-      export npm_config_nodedir=${nodedir}
-      export RN_WEBRTC_SKIP_DOWNLOAD=1
-    '';
-    yarnFlags = remove "--ignore-scripts" yarn2nix-moretea.defaultYarnFlags;
-    pkgConfig.default.buildInputs = [ nodePackages.node-pre-gyp python3 pkg-config pango nodePackages.patch-package ];
-  };
+  node_modules = runCommand "node_modules" { nativeBuildInputs = [ yarn nodejs python3 pkg-config gcc pango ]; } ''
+    cp ${sources.jitsi-meet}/package.json .
+    cp ${./yarn.lock} yarn.lock
+    chmod +w yarn.lock
+    ${yarn2nix-moretea.fixup_yarn_lock}/bin/fixup_yarn_lock yarn.lock
+    export HOME=$PWD/yarn_home
+    yarn config --offline set yarn-offline-mirror ${yarn2nix-moretea.importOfflineCache ./yarn.nix}
+    yarn --offline --ignore-scripts --modules-folder $out
+    cd $out
+    patchShebangs .
+    export RN_WEBRTC_SKIP_DOWNLOAD=1
+    npm rebuild --nodedir=${nodedir}
+  '';
   jitsi-meet-source-package = stdenv.mkDerivation {
     name = "jitsi-meet.tar.bz2";
     inherit src;
     patches = [ ./changes.patch ];
-    buildInputs = [ nodejs ];
+    nativeBuildInputs = [ nodejs ];
     buildPhase = ''
-      ln -s ${yarnModules}/node_modules .
+      cp -r ${node_modules} node_modules
       ./node_modules/.bin/webpack
       make deploy source-package
     '';
