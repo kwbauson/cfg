ddaa809cfec86720602273d3e693fa98c76e0faa
Keith Bauson <kwbauson@gmail.com>
2022-05-06 16:32:33 -0400

    make firefox default

--- home.nix
+++ home.nix
@@ -545,7 +545,29 @@ with builtins; with pkgs; with mylib; {
   };
   xdg = {
     enable = true;
-    ${attrIf isNixOS "mimeApps"}.enable = true;
+    ${attrIf isNixOS "mimeApps"} =
+      let
+        firefox-associations = listToAttrs (
+          map (mime: nameValuePair mime "firefox.desktop")
+            [
+              "x-scheme-handler/http"
+              "x-scheme-handler/https"
+              "x-scheme-handler/chrome"
+              "text/html"
+              "application/x-extension-htm"
+              "application/x-extension-html"
+              "application/x-extension-shtml"
+              "application/xhtml+xml"
+              "application/x-extension-xhtml"
+              "application/x-extension-xht"
+            ]
+        );
+      in
+      {
+        enable = true;
+        associations.added = firefox-associations;
+        defaultApplications = firefox-associations;
+      };
     ${attrIf isNixOS "userDirs"} = {
       enable = true;
       desktop = "$HOME";
