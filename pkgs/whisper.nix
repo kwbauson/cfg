e05a9c110ddaefca66dc6bf530d2542c00017805
Keith Bauson <kwbauson@gmail.com>
2022-09-27 18:24:15 -0400

    drop whisper since it's in nixpkgs

--- pkgs/whisper.nix
+++
@@ -1,8 +0,0 @@
-scope: with scope; with python3Packages; buildPythonPackage {
-  inherit pname version src;
-  propagatedBuildInputs = [ ffmpeg-python tqdm torch more-itertools transformers ];
-  doCheck = false;
-  postInstall = ''
-    wrapProgram $out/bin/${pname} --set PATH ${makeBinPath [ ffmpeg_5-full ]}
-  '';
-}
