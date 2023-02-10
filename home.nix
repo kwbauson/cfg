2515b080f029156e7524dcd4310b166fa63f57ca
Keith Bauson <kwbauson@gmail.com>
2023-02-10 09:21:07 -0500

    tmp disable ncdu for macos

--- home.nix
+++ home.nix
@@ -60,7 +60,7 @@ with builtins; with pkgs; with mylib; {
           inherit i3-easyfocus iproute2 iputils pavucontrol strace sway dzen2
             maim zoom-us acpi usbutils xdotool qtbr signal-desktop discord;
           inherit breeze-icons nixos-install-tools arduino;
-          inherit progress niv ffmpeg-full yt-dlp;
+          inherit progress niv ffmpeg-full yt-dlp ncdu;
         };
       };
 
