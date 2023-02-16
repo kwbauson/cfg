531e24f774f9deb2edd06dad6e1e32b8abbf2c4a
Keith Bauson <kwbauson@gmail.com>
2023-02-16 10:56:05 -0500

    manage authorized_keys keys with home-manager

--- home.nix
+++ home.nix
@@ -700,6 +700,7 @@ with builtins; with pkgs; with mylib; {
     IRB.conf[:SAVE_HISTORY] = 2_000_000
     IRB.conf[:HISTORY_FILE] = "#{ENV['XDG_DATA_HOME']}/irb_history"
   '';
+  home.file.".ssh/authorized_keys".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/cfg/authorized_keys";
 
   gtk.enable = isLinux;
   gtk.theme.name = "Adwaita-dark";
