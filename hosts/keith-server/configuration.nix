ec2b08c8183366463a4f2b0c358a415f4e26715a
Keith Bauson <kwbauson@gmail.com>
2023-02-16 21:46:11 -0500

    add server

---
+++ hosts/keith-server/configuration.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, self, ... }:
+with builtins;
+{
+  imports = with self.inputs.nixos-hardware.nixosModules; [
+    ./hardware-configuration.nix
+    common-cpu-amd
+    common-gpu-amd
+    common-cpu-amd-pstate
+  ];
+
+  nix.settings.trusted-users = [ "@wheel" ];
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.systemd-boot.configurationLimit = 3;
+  hardware.amdgpu.loadInInitrd = false;
+
+  time.hardwareClockInLocalTime = true;
+  time.timeZone = "America/Indianapolis";
+
+  services.localtimed.enable = false;
+  services.xserver.enable = true;
+  virtualisation.docker.enable = true;
+  users.users.keith.extraGroups = [ "dialout" ];
+  services.tailscale.enable = true;
+  services.openssh.enable = true;
+}
