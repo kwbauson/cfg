25fb8a677f3b599471c465d759d626b6c266abe1
Keith Bauson <kwbauson@gmail.com>
2023-02-20 23:53:56 -0500

    hopefully this port forwarding is better

--- machines/kwbauson/configuration.nix
+++ machines/kwbauson/configuration.nix
@@ -15,18 +15,25 @@
     inherit (kwbauson) domain;
     firewall.allowedTCPPorts = [ http.port https.port jitsi.tcp-port ] ++ valheim.ports;
     firewall.allowedUDPPorts = [ jitsi.udp-port ] ++ valheim.ports;
-    # firewall.checkReversePath = false;
+    firewall.checkReversePath = false;
     firewall.trustedInterfaces = [ "tailscale0" ];
-    # nat.enable = true;
-    # nat.externalIP = kwbauson.ip;
-    # nat.internalIPs = [ keith-server.ip ];
-    # nat.externalInterface = "enp3s0";
-    # nat.internalInterfaces = [ "tailscale0" ];
-    # nat.forwardPorts =
-    #   map (port: { proto = "tcp"; sourcePort = port; destination = "${keith-server.ip}:${toString port}"; })
-    #     ([ jitsi.tcp-port ] ++ valheim.ports) ++
-    #   map (port: { proto = "udp"; sourcePort = port; destination = "${keith-server.ip}:${toString port}"; })
-    #     ([ jitsi.udp-port ] ++ valheim.ports);
+    firewall.extraCommands = concatMapStringsSep "\n"
+      ({ port, proto }: "iptables -t nat -A POSTROUTING -d ${keith-server.ip} -p ${proto} -m ${proto} --dport ${toString port} -j MASQUERADE")
+      (
+        (map (port: { proto = "tcp"; inherit port; }) valheim.ports) ++
+        (map (port: { proto = "udp"; inherit port; }) valheim.ports) ++
+        [{ proto = "tcp"; port = jitsi.tcp-port; } { proto = "udp"; port = jitsi.udp-port; }]
+      );
+    nat.enable = true;
+    nat.externalIP = kwbauson.ip;
+    nat.internalIPs = [ keith-server.ip ];
+    nat.externalInterface = "enp3s0";
+    nat.internalInterfaces = [ "tailscale0" ];
+    nat.forwardPorts =
+      map (port: { proto = "tcp"; sourcePort = port; destination = "${keith-server.ip}:${toString port}"; })
+        ([ jitsi.tcp-port ] ++ valheim.ports) ++
+      map (port: { proto = "udp"; sourcePort = port; destination = "${keith-server.ip}:${toString port}"; })
+        ([ jitsi.udp-port ] ++ valheim.ports);
   };
 
   services.openssh.enable = true;
@@ -38,22 +45,5 @@
     "files.${fqdn}".extraConfig = "reverse_proxy keith-server:${toString file-server.port}";
     "netdata.${fqdn}".extraConfig = "reverse_proxy keith-server:${toString netdata.port}";
     "jitsi.${fqdn}".extraConfig = "reverse_proxy keith-server:${toString jitsi.caddy-port}";
-  };
-
-  systemd.services.forward-ports = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ socat ];
-    script = ''
-      # valheim
-      for port in 2456 2457;do
-        for proto in TCP UDP;do
-          socat $proto-LISTEN:$port,fork,reuseaddr $proto:keith-server:$port &
-        done
-      done
-      # jitsi
-      socat TCP-LISTEN:4443,fork,reuseaddr TCP:keith-server:4443 &
-      socat UDP-LISTEN:10000,fork,reuseaddr UDP:keith-server:10000 &
-      sleep inf
-    '';
   };
 }
