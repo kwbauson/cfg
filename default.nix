552cd9edcc73a5d39f156e2a615741f301a1e9b1
Keith Bauson <kwbauson@gmail.com>
2023-04-01 13:00:01 -0400

    did i fix nle?

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-with builtins; let
+with builtins;
+{ system ? currentSystem, forceFlakeCompat ? true }:
+let
   lock = fromJSON (readFile ./flake.lock);
   flake-compat = with lock.nodes.flake-compat.locked; import (fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
     sha256 = narHash;
   });
-  getFlake = src: (flake-compat { inherit src; }).defaultNix;
+  getFlake = src:
+    if builtins ? getFlake && !forceFlakeCompat
+    then builtins.getFlake (toString src)
+    else (flake-compat { inherit src; }).defaultNix;
 in
-{ system ? currentSystem }:
 (getFlake ./.).packages.${system}.scope // { inherit getFlake; }
