c43877c82093d62d3ccd503d037675ad7dfcc748
Keith Bauson <kwbauson@gmail.com>
2023-04-10 22:14:46 -0400

    ope again

--- overlays/ci-checks.nix
+++ overlays/ci-checks.nix
@@ -2,7 +2,7 @@ final: prev: {
   ci-checks = with final.scope; let
     mkCheck = f: attrs: writeBashBin "ci-checks" ''
       echo ${linkFarm "ci-build" (mapAttrValues f attrs)}
-      ${better-comma}/bin/, hello
+      ${better-comma}/bin/, -p hello hello
     '';
   in
   {
