5553196e23be4cfe5f25f0ef8813102ad372fa7b
Keith Bauson <kwbauson@gmail.com>
2023-04-10 22:16:26 -0400

    check better comma in ci

--- overlays/ci-checks.nix
+++ overlays/ci-checks.nix
@@ -2,6 +2,7 @@ final: prev: {
   ci-checks = with final.scope; let
     mkCheck = f: attrs: writeBashBin "ci-checks" ''
       echo ${linkFarm "ci-build" (mapAttrValues f attrs)}
+      ${better-comma}/bin/, -p hello hello
     '';
   in
   {
