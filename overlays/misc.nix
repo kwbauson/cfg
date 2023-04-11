d61a05526f02bbe9a324806e2d99e58deed64ce3
Keith Bauson <kwbauson@gmail.com>
2023-04-10 22:09:41 -0400

    trying better self flake lock

--- overlays/misc.nix
+++ overlays/misc.nix
@@ -66,36 +66,49 @@
     contentAddressed = mapAttrs (_: pkg: if pkg ? overrideAttrs then pkg.overrideAttrs (_: { __contentAddressed = true; }) else pkg) pkgs;
     npmlock2nix = import sources.npmlock2nix { inherit pkgs; };
     devenv = (import sources.devenv).packages.${system}.default;
-    self-flake-lock = runCommand "self-flake-lock" { nativeBuildInputs = [ jq moreutils ]; } ''
-      cp ${scope.self-source}/flake.lock $out
-      chmod +w $out
-      entries=$(jq '.nodes.root.inputs | to_entries' $out)
-      inputs_keys=$(jq -r '.[].key' <<<"$entries")
-      inputs_values=$(jq -r '.[].value' <<<"$entries")
-      if [[ $inputs_keys != $inputs_values ]];then
-        echo invalid input mapping
-        jq '.nodes.root.inputs' $out
-        echo generated flake.lock of self does not support nested inputs
-        exit 1
-      fi
-      self_inputs="${concatStringsSep "\n" (attrNames scope.inputs)}"
-      self_inputs=$(echo "$self_inputs" | sort)
-      lock_inputs=$(jq -r '.nodes | keys | sort[]' $out | grep -vFx root)
-      if [[ $inputs_keys != $lock_inputs ]];then
-        echo self_inputs: $self_inputs
-        echo lock_inputs: $lock_inputs
-        echo inputs of self do not match flake.lock nodes
-        exit 1
-      fi
-      self_inputs="${concatStringsSep "\n" (mapAttrsToList (n: v: "${n} ${v.outPath} ${v.narHash}") scope.inputs)}"
-      echo "$self_inputs" | while read input outPath narHash;do
-        jq ".nodes.\"$input\".locked = { type: \"path\", path: \"$outPath\", narHash: \"$narHash\" }" $out | sponge $out
-      done
-    '';
-    self-flake = runCommand "self-flake" { } ''
+    selfFlakeLock = {
+      version = 7;
+      root = "root";
+      nodes = {
+        root = {
+          inputs = genAttrs (remove "root" (attrNames scope.selfFlakeLock.nodes)) id;
+        };
+      } // (
+        let
+          toName = parents: input: concatStringsSep "_" (parents ++ [ input ]);
+          gen = parents: concatMapAttrs (name: value:
+            let
+              newParents = parents ++ [ name ];
+              isFlake = (value.type or null) == "_flake";
+              hasInputs = (value.inputs or { }) != { };
+            in
+            {
+              ${toName parents name} = {
+                locked = {
+                  type = "path";
+                  path = value.outPath;
+                  inherit (value) narHash;
+                };
+                original = {
+                  id = toName parents name;
+                  type = "indirect";
+                };
+              }
+              // optionalAttrs isFlake { flake = false; }
+              // optionalAttrs hasInputs { inputs = genAttrs (attrNames value.inputs) (toName newParents); };
+            } // gen newParents (value.inputs or { }));
+        in
+        gen [ ] scope.inputs
+      );
+    };
+    self-flake = runCommand "self-flake"
+      {
+        selfFlakeLock = toJSON scope.selfFlakeLock;
+        passAsFile = [ "selfFlakeLock" ];
+      } ''
       cp -r ${scope.self-source} $out
       chmod -R +w $out
-      cp ${scope.self-flake-lock} $out/flake.lock
+      cp $selfFlakeLockPath $out/flake.lock
     '';
     iso = with scope.packages.x86_64-linux; (nixos ({ modulesPath, ... }: {
       imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix" ];
