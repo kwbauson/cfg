60ad12c27db0b033453372971a3ab4f126b5baa3
Keith Bauson <kwbauson@gmail.com>
2023-05-15 15:47:20 -0400

    Automatic update (#1497)
    * Automatic update
    
    * disable these patches, they might be upstreamed and i don't really use it anyways
    
    ---------
    
    Co-authored-by: kwbauson <kwbauson@users.noreply.github.com>

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,15 +6,6 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    package = neovim-unwrapped.overrideAttrs (old: {
-      patches = old.patches or [ ] ++ [
-        (fetchpatch {
-          name = "use-the-correct-replacement-args-for-gsub-directive.patch";
-          url = "https://github.com/neovim/neovim/commit/ccc0980f86c6ef9a86b0e5a3a691f37cea8eb776.patch";
-          hash = "sha256-sZWM6M8jCL1e72H0bAc51a6FrH0mFFqTV1gGLwKT7Zo=";
-        })
-      ];
-    });
     extraPackages = attrValues { inherit nimlsp nil solargraph terraform-ls lua-language-server; };
     withNodeJs = true;
     coc.enable = true;
@@ -28,15 +19,7 @@
         coc-eslint coc-git coc-json coc-lists coc-prettier
         coc-solargraph coc-tsserver coc-pyright coc-explorer
         coc-vetur coc-vimlsp;
-      nvim-treesitter = nvim-treesitter.withAllGrammars.overrideAttrs (old: {
-        patches = old.patches or [ ] ++ [
-          (fetchpatch {
-            name = "injections-nix-dynamic-language-injection-via-comments.patch";
-            url = "https://github.com/nvim-treesitter/nvim-treesitter/pull/4658/commits/49d018d0b43ae260a5f182e090ad816c50edb3e4.patch";
-            hash = "sha256-+pUQ+opKub3oy5t/KD1Qgwn6avieFeMANq+MZqNQpNc=";
-          })
-        ];
-      });
+      nvim-treesitter = nvim-treesitter.withAllGrammars;
     };
     extraConfig = "
       source ${config.home.homeDirectory}/cfg/init.vim
