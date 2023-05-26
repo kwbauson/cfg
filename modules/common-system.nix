9546aa36f941718b59a2077b74a471413494e035
Keith Bauson <kwbauson@gmail.com>
2023-05-26 19:34:29 -0400

    common system config between nixos and nix-darwin (#1507)
    * common system config between nixos and nix-darwin
    
    * oops
    
    * try ssh keys

---
+++ modules/common-system.nix
@@ -0,0 +1,20 @@
+{ scope, machine-name, username, ... }: with scope;
+{
+  _module.args.username = lib.mkDefault "keith";
+  environment.etc."nixpkgs-path".source = pkgs.path;
+  nix.nixPath = [ "nixpkgs=/etc/nixpkgs-path" ];
+  nix.settings.trusted-users = [ "@wheel" ];
+  nix.extraOptions = nixConf;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    extraSpecialArgs = { inherit scope machine-name; };
+    users.${username}.imports = [ modules.home-manager ];
+  };
+
+  users.users.${username}.openssh.authorizedKeys.keys = pipe ../authorized_keys [
+    readFile
+    (splitString "\n")
+    (filter (x: x != ""))
+  ];
+}
