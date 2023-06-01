1ed89e0ab6149c106e5164933af1d21b7e3eb011
Keith Bauson <kwbauson@gmail.com>
2023-06-01 08:13:02 -0400

    hopefully fix bws updates

--- pkgs/bws/default.nix
+++ pkgs/bws/default.nix
@@ -8,11 +8,12 @@ scope: with scope; rustPlatform.buildRustPackage {
   buildInputs = [ openssl ] ++ optionals isDarwin [ darwin.Security ];
 
   passthru.updateScript = writeBashBin "update-bws" ''
+    set -euo pipefail
     cd pkgs/bws
     rev=$(${exe jq} -r .rev < cargo.json)
     newRev=${sources.bitwarden-sdk.rev}
     if [[ $rev != $newRev ]];then
-      hash=$(${exe nix-prefetch} -f ${pkgs.path} '{ sha256 }: (import ${flake} {}).${pname}.cargoDeps.overrideAttrs (_: { cargoSha256 = sha256; })')
+      hash=$(NIX_PATH=nixpkgs=${pkgs.path} ${exe nix-prefetch} '{ sha256 }: (import ${flake} {}).${pname}.cargoDeps.overrideAttrs (_: { cargoSha256 = sha256; })')
       ${exe jo} -p rev="$newRev" hash="$hash" -o cargo.json
     fi
   '';
