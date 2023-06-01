5027ebf8a2fc42bcb73f28584935d57fb1da2057
Keith Bauson <kwbauson@gmail.com>
2023-06-01 08:39:10 -0400

    fakes3 needs ruby 2

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 
 gem 'rb-inotify'
 gem 'fakes3'
-gem 'webrick'
--- gemset.nix
+++ gemset.nix
@@ -61,16 +61,6 @@
     };
     version = "1.2.2";
   };
-  webrick = {
-    groups = ["default"];
-    platforms = [];
-    source = {
-      remotes = ["https://rubygems.org"];
-      sha256 = "13qm7s0gr2pmfcl7dxrmq38asaza4w0i2n9my4yzs499j731wh8r";
-      type = "gem";
-    };
-    version = "1.8.1";
-  };
   xml-simple = {
     dependencies = ["rexml"];
     groups = ["default"];
