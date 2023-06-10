ec0035e843a4b2207255056083feeea65f367593
Keith Bauson <kwbauson@gmail.com>
2023-06-10 19:12:31 -0400

    use nix-darwin for github runners

--- machines/readlee-mac-m1/darwin-configuration.nix
+++ machines/readlee-mac-m1/darwin-configuration.nix
@@ -1,3 +1,24 @@
+{ config, scope, ... }: with scope;
+let
+  github-runners-fork = fetchTree {
+    type = "github";
+    owner = "DavHau";
+    repo = "nix-darwin";
+    ref = "github-runners";
+    narHash = "sha256-z9PpdTZRRaAXM6eFznIgbNCRp9jQoIAQ67k0YDxvn/A=";
+  };
+in
 {
+  imports = [ "${github-runners-fork}/modules/services/github-runners" ];
   _module.args.username = "benjamin";
+
+  services.github-runners = mapAttrs' (n: nameValuePair "runner-${n}") {
+    kwbauson-cfg = {
+      url = "https://github.com/kwbauson/cfg";
+      tokenFile = "/etc/github-runner.token";
+      extraLabels = [ "nix" ];
+      extraPackages = [ nix gh ];
+    };
+  };
+  launchd.daemons = forAttrValues config.services.github-runners (const { path = [ coreutils ]; });
 }
