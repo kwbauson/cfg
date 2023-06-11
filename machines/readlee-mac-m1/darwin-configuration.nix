a6520d51d4646df71ed64d1583a8543bbae19737
Keith Bauson <kwbauson@gmail.com>
2023-06-10 20:41:30 -0400

    move the readlee runners

--- machines/readlee-mac-m1/darwin-configuration.nix
+++ machines/readlee-mac-m1/darwin-configuration.nix
@@ -7,6 +7,16 @@ let
     ref = "github-runners";
     narHash = "sha256-z9PpdTZRRaAXM6eFznIgbNCRp9jQoIAQ67k0YDxvn/A=";
   };
+  readlee-runner-defaults = {
+    url = "https://github.com/paper-co/readlee";
+    extraLabels = [ "nix" "readlee" ];
+    extraPackages = [
+      (writeBashBin "run-job-script" ''
+        export PATH="$PATH":/usr/bin:/usr/sbin:/bin:/sbin
+        ${bash}/bin/bash github-workdir/job-script.sh
+      '')
+    ];
+  };
 in
 {
   imports = [ "${github-runners-fork}/modules/services/github-runners" ];
@@ -19,15 +29,33 @@ in
       extraLabels = [ "nix" ];
       extraPackages = [ gh cachix ];
     };
+    # benaduggan-nix = {
+    #   url = "https://github.com/benaduggan/nix";
+    #   tokenFile = "/etc/github-runner-benaduggan-nix.token";
+    # };
+    readlee-arm64 = readlee-runner-defaults // {
+      tokenFile = "/etc/github-runner-readlee-arm64.token";
+    };
+    readlee-x64 = readlee-runner-defaults // {
+      package = pkgsx86_64Darwin.github-runner;
+      tokenFile = "/etc/github-runner-readlee-x64.token";
+    };
   };
   launchd.daemons = forAttrValues config.services.github-runners (cfg: {
-    # fixes copied from the nixos module
+    # daemon path fixes copied from the nixos module
     path = [
       bash
       coreutils
       git
       gnutar
       gzip
-    ] ++ [ config.nix.package ] ++ cfg.extraPackages;
+    ] ++ [
+      (
+        # allow x64 runners
+        if cfg.package != pkgsx86_64Darwin.github-runner
+        then config.nix.package
+        else pkgsx86_64Darwin.nix
+      )
+    ] ++ cfg.extraPackages;
   });
 }
