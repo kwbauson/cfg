f01eb9a12ec1f2ae3c8b3777616de5f2268bc0d9
Keith Bauson <kwbauson@gmail.com>
2023-06-12 13:13:53 -0400

    use qutebrowser-qt6

--- pkgs/qtbr.nix
+++ pkgs/qtbr.nix
@@ -1,7 +1,7 @@
 scope: with scope; runBin pname ''
-  cat ${qutebrowser}/share/qutebrowser/scripts/open_url_in_instance.sh |
+  cat ${qutebrowser-qt6}/share/qutebrowser/scripts/open_url_in_instance.sh |
     sed 's/_url="$1"/_url="$\{1-about:blank}"/' |
     ${optionalString isDarwin "sed 's:_ipc_socket=.*:_ipc_socket=/tmp/qutebrowser-socket:' |"}
     sed 's/"$_qute_bin"/exec "$_qute_bin"/' |
-    sed 's:^_qute_bin=.*:_qute_bin=${qutebrowser}/bin/qutebrowser:'
+    sed 's:^_qute_bin=.*:_qute_bin=${qutebrowser-qt6}/bin/qutebrowser:'
 ''
