7edfee35b5fb24164e6e948c4f6c555470cbb738
Keith Bauson <kwbauson@gmail.com>
2023-06-19 12:09:50 -0400

    pr to revert netdata fixes

--- modules/nixos/netdata-fixes.nix
+++
@@ -1,5 +0,0 @@
-{ config, options, ... }:
-{
-  options.services.netdata.claim_token_file = options.services.netdata.claimTokenFile;
-  config.services.netdata.claim_token_file = config.services.netdata.claimTokenFile;
-}
