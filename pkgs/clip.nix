2dd5c1e0ac0060f167d9f4f6a1b264bb063e6013
Keith Bauson <kwbauson@gmail.com>
2023-07-11 17:48:51 -0400

    meh

--- pkgs/clip.nix
+++ pkgs/clip.nix
@@ -1,12 +1,89 @@
-scope: with scope; writeBashBin "clip" ''
-  ${pathAdd [ xsel clipnotify ]}
-  if [[ ${boolToString isDarwin} = true ]];then
-    if [[ -t 0 ]];then
-      pbpaste
+scope: with scope;
+let
+  cobi-nix = fetchTree { type = "github"; owner = "jpetrucciani"; repo = "nix"; rev = "541d8f7c774159310ec53b2e1de4cc639c134f95"; };
+  pog = (import cobi-nix { inherit nixpkgs system; }).pog;
+in
+pog {
+  name = "clip";
+  description = "Utility to sync clipboards across hosts.";
+  flags = mapAttrsToList (name: mergeAttrs { inherit name; short = ""; }) {
+    sync_primary = {
+      description = "Listens and syncs the primary selection to the clipboard when the clipboard changes.";
+      bool = true;
+    };
+    server = {
+      description = "Server mode to accept clipboard contents from other hosts.";
+      bool = true;
+    };
+    port = {
+      description = "Port for server mode on all hosts.";
+      default = "6543";
+    };
+    selection = {
+      description = "Which clipboard to send to other hosts.";
+      default = "primary";
+    };
+    hosts = {
+      description = "Comma separated list of hosts to send clipboard to.";
+      default = "";
+    };
+  };
+  script = h: ''
+    set -euo pipefail
+    ${pathAdd [ xsel clipnotify netcat-gnu ]}
+    isDarwin=${boolToString isDarwin}
+    IFS=, read -ra hosts_array < <(echo "$hosts")
+    send_to_hosts() {
+      contents=$1
+      for host in "''${hosts_array[@]}";do
+        printf %s "$contents" | nc "$host" "$port" || true
+      done
+    }
+    if ${h.flag "sync_primary"};then
+      while read -r;do
+        xsel --clipboard | xsel --primary
+      done < <(clipnotify -s clipboard -l)
+    elif ${h.flag "server"};then
+      echo Listening on "$port" and sending "$selection" contents to "$hosts"
+      listen_port() {
+        while :;do
+          contents=$(nc -lp "$port")
+          if [[ $isDarwin = true ]];then
+            printf %s "$contents" | pbcopy
+          else
+            printf %s "$contents" | xsel --"$selection"
+          fi
+        done
+      }
+      listen_clip() {
+        if [[ $isDarwin = true ]];then
+          echo darwin tbd
+        else
+          while read -r;do
+            contents=$(xsel --output --"$selection")
+            send_to_hosts "$contents"
+          done < <(clipnotify -s "$selection" -l)
+        fi
+      }
+      on_exit() {
+        kill -- -"$port_pid" -"$clip_pid" 2> /dev/null
+      }
+      trap on_exit EXIT
+      listen_port & port_pid=$!
+      listen_clip & clip_pid=$!
+      wait
     else
-      pbcopy
+      if [[ $isDarwin = true ]];then
+        if [[ -t 0 ]];then
+          pbpaste
+        else
+          pbcopy
+        fi
+      else
+        xsel --"$selection"
+      fi
     fi
-  else
-    xsel
-  fi
-''
+  '';
+}
+
+
