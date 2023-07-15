82294c0403332ee9a1bfbb422bba193a1d6a185d
Keith Bauson <kwbauson@gmail.com>
2023-07-15 11:04:09 -0400

    Revert "try using patchModules for darwin github runner modules"
    This reverts commit a9910ec5cae902fdefad50c7f2ccd5ccf7bd29d4.

--- machines/readlee-mac-m1/darwin-configuration.nix
+++ machines/readlee-mac-m1/darwin-configuration.nix
@@ -1,5 +1,12 @@
 { config, scope, ... }: with scope;
 let
+  github-runners-fork = fetchTree {
+    type = "github";
+    owner = "DavHau";
+    repo = "nix-darwin";
+    ref = "github-runners";
+    narHash = "sha256-z9PpdTZRRaAXM6eFznIgbNCRp9jQoIAQ67k0YDxvn/A=";
+  };
   readlee-runner-defaults = {
     url = "https://github.com/paper-co/readlee";
     extraLabels = [ "nix" "readlee" ];
@@ -12,14 +19,7 @@ let
   };
 in
 {
-  imports = [
-    (patchModules inputs.nix-darwin [
-      (fetchpatch {
-        url = "https://patch-diff.githubusercontent.com/raw/LnL7/nix-darwin/pull/604.patch";
-        hash = "sha256-RmVbqp1lOGNioEU8NuXt/SP5eSXyITdzba9hfFrmwVM=";
-      })
-    ])
-  ];
+  imports = [ "${github-runners-fork}/modules/services/github-runners" ];
   _module.args.username = "benjamin";
 
   services.github-runners = mapAttrs' (n: value: nameValuePair "runner-${n}" (value // { replace = true; })) {
