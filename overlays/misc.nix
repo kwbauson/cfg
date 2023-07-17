7555f21cd14b15c3926d283e15b4b67b13ae46a1
Keith Bauson <kwbauson@gmail.com>
2023-07-17 12:11:54 -0400

    stop using old nix for nixos-option

--- overlays/misc.nix
+++ overlays/misc.nix
@@ -1,7 +1,6 @@
 final: prev: with final.scope; {
   nix = nixUnstable;
   nixOld = prev.nix;
-  nixos-option = prev.nixos-option.override { nix = nixOld; };
   nix-wrapped = wrapBins nix ''
     mkdir -p ~/.local/share/nix
     export NIX_CONFIG=$(< ${writeText "nix.conf" nixConfBase})$'\n'$NIX_CONFIG
