d51d191ece884f54e41489e50f0e3b4b230f00ea
Keith Bauson <kwbauson@gmail.com>
2023-07-23 14:20:55 -0400

    swarm

--- pkgs/swarm.nix
+++ pkgs/swarm.nix
@@ -1,10 +1,19 @@
 scope: with scope;
-let
-  haskell-nix-flake = (import inputs.flake-compat { src = sources.haskell-nix; }).defaultNix;
-  haskell-nix = haskell-nix-flake.legacyPackages.${system}.haskell-nix;
-  project = haskell-nix.project {
-    src = src // { name = "swarm"; };
-    projectFileName = "stack.yaml";
+importPackage (attrs: {
+  inherit pname;
+  version = "unstable-2023-07-22";
+  src = fetchFromGitHub {
+    owner = "swarm-game";
+    repo = pname;
+    rev = "f9c22635b5160c0610683d55b387f7157f6ee8f5";
+    hash = "sha256-n0EwdtpJPSItvf6e6h5t9lXGHuVyVG3KILPlUPGTreM=";
   };
-in
-{ updateScript = [ ]; }
+  package =
+    let
+      haskell-nix-flake = (import inputs.flake-compat { src = sources.haskell-nix; }).defaultNix;
+      haskell-nix = haskell-nix-flake.legacyPackages.${system}.haskell-nix;
+      project = haskell-nix.project { src = attrs.src // { name = "swarm"; }; projectFileName = "stack.yaml"; };
+    in
+    project.swarm.components.exes.swarm;
+  passthru.updateScript = unstableGitUpdater { };
+})
