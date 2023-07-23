ecff03891f5397107d83e5bac37d1ce82ca08339
Keith Bauson <kwbauson@gmail.com>
2023-07-23 15:37:20 -0400

    jitsi-meet

--- pkgs/jitsi-meet/default.nix
+++ pkgs/jitsi-meet/default.nix
@@ -1,7 +1,13 @@
 scope: with scope;
 let
+  src = fetchFromGitHub {
+    owner = "jitsi";
+    repo = pname;
+    rev = "e1ac000cd1f15642218e80ded98ee19188cf2b17";
+    sha256 = "sha256-7wKpUYm6KxNy4W8i4Hcctw6jSiV0+gbz0FnuEcqmjpM=";
+  };
   node_modules = mkYarnModulesWithRebuild {
-    packageJSON = "${sources.jitsi-meet}/package.json";
+    packageJSON = "${src}/package.json";
     yarnLock = ./yarn.lock;
     extraNativeBuildInputs = [ pango ];
     preRebuild = "export RN_WEBRTC_SKIP_DOWNLOAD=1";
@@ -23,13 +29,15 @@ let
 in
 jitsi-meet.overrideAttrs (_: {
   src = jitsi-meet-source-package;
-  passthru.updateScript = [ ];
+  passthru = { inherit jitsi-meet-source-package; };
+  passthru.jitsi-src = src;
+  passthru.updateScript = null;
   # passthru.updateScript = writeBashBin "update-jitsi" ''
   #   ${ pathAdd [ yarn coreutils ] }
   #   set -eo pipefail
   #   dir=$(mktemp -d /tmp/update-jitsi.XXXXX)
   #   cd "$dir"
-  #   cp --no-preserve=mode ${sources.jitsi-meet}/package{,-lock}.json .
+  #   cp --no-preserve=mode ${src.jitsi-meet}/package{,-lock}.json .
   #   yarn import --ignore-engines
   #   cd -
   #   cp "$dir"/yarn.lock pkgs/jitsi-meet
