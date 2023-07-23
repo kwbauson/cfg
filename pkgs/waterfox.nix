0c6b4778f90306aae9a9aa9fa2b54dc05ef79bac
Keith Bauson <kwbauson@gmail.com>
2023-07-23 16:19:35 -0400

    fix waterfox

--- pkgs/waterfox.nix
+++ pkgs/waterfox.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation (attrs: {
   version = "G4.1.5";
   src = fetchurl {
     url = "https://cdn.waterfox.net/releases/linux64/installer/${attrs.pname}-${attrs.version}.en-US.linux-x86_64.tar.bz2";
-    hash = "sha256-TODO";
+    hash = "sha256-E/PFhKkMUK0P6QBtH+CQbt/aoCztmfDwJwkNIm7Q+UI=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = firefox-unwrapped.buildInputs ++ [ gtk2 ];
@@ -20,5 +20,6 @@ stdenv.mkDerivation (attrs: {
       sort -V |
       tail -n1
     )"
+    ${getExe nix-update} --flake --version skip waterfox
   '';
 })
