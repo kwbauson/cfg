f71f6c9a02c057c22cb03bac6bda13cbd5ee414d
Keith Bauson <kwbauson@gmail.com>
2023-07-23 13:11:50 -0400

    git-trim

--- pkgs/git-trim.nix
+++ pkgs/git-trim.nix
@@ -1,4 +1,12 @@
 scope: with scope; stdenv.mkDerivation {
-  inherit pname version src;
+  inherit pname;
+  version = "unstable-2013-08-14";
+  src = fetchFromGitHub {
+    owner = "dstnbrkr";
+    repo = pname;
+    rev = "68322f146a443937e8f59aab4fa3dd0eb976a4db";
+    hash = "sha256-AWxB4JS+L6XqD9jaFofoMCNQAsRIOQ4nv4Jo6D40P/g=";
+  };
   installPhase = "install -Dt $out/bin $src/${pname}";
+  passthru.updateScript = unstableGitUpdater { };
 }
