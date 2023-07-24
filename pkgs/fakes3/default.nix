ff41be12d49701348f87068e13e78240fcaa7870
Keith Bauson <kwbauson@gmail.com>
2023-07-24 16:19:07 -0400

    move fakes3 to its own package

---
+++ pkgs/fakes3/default.nix
@@ -0,0 +1,13 @@
+scope: with scope;
+bundlerApp {
+  inherit pname;
+  ruby = ruby_2_7;
+  gemdir = ./.;
+  exes = [ pname ];
+  passthru.updateScript = writeShellScript "update" ''
+    ${pathAdd [ bundler bundix ]}
+    cd ~/cfg/pkgs/fakes3
+    bundler lock --update
+    bundix
+  '';
+}
