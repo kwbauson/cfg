6d64574abb13e69d95595659307ca4c3fd17e3d1
Keith Bauson <kwbauson@gmail.com>
2023-07-24 08:51:12 -0400

    another lib usage

--- pkgs/qtbr.nix
+++ pkgs/qtbr.nix
@@ -1,9 +1,10 @@
-scope: with scope; (runBin pname ''
-  cat ${qutebrowser-qt6}/share/qutebrowser/scripts/open_url_in_instance.sh |
-    sed 's/_url="$1"/_url="$\{1-about:blank}"/' |
-    ${optionalString isDarwin "sed 's:_ipc_socket=.*:_ipc_socket=/tmp/qutebrowser-socket:' |"}
-    sed 's/"$_qute_bin"/exec "$_qute_bin"/' |
-    sed 's:^_qute_bin=.*:_qute_bin=${qutebrowser-qt6}/bin/qutebrowser:'
-'').overrideAttrs (_: {
-  meta.platforms = platforms.linux;
-})
+scope: with scope;
+addMetaAttrs { platforms = platforms.linux; } (
+  runBin pname ''
+    cat ${qutebrowser-qt6}/share/qutebrowser/scripts/open_url_in_instance.sh |
+      sed 's/_url="$1"/_url="$\{1-about:blank}"/' |
+      ${optionalString isDarwin "sed 's:_ipc_socket=.*:_ipc_socket=/tmp/qutebrowser-socket:' |"}
+      sed 's/"$_qute_bin"/exec "$_qute_bin"/' |
+      sed 's:^_qute_bin=.*:_qute_bin=${qutebrowser-qt6}/bin/qutebrowser:'
+  ''
+)
