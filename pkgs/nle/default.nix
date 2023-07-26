81991995cf39c08e44e3cc0366da92d210efc40f
Keith Bauson <kwbauson@gmail.com>
2023-07-26 15:01:49 -0400

    nle: drop update

--- pkgs/nle/default.nix
+++ pkgs/nle/default.nix
@@ -3,54 +3,9 @@ let
   build = let nixpkgs = pkgs; in
     { path, pkgs ? nixpkgs, source ? null }:
     import ./nix-local-env.nix { inherit pkgs path source; };
-  joinMapAttrValuesIf = f: p: as: concatMapStringsSep "\n" f (attrValues (filterAttrs (n: v: p n) as));
-  scripts = makeScripts {
-    update = ''
-      set -e
-      ${joinMapAttrValuesIf nrs (hasPrefix "update-") scripts}
-    '';
-    update-npm = ''
-      ${pathAdd [ nodejs_latest nodePackages.npm-check-updates ]}
-      if [[ -e package.json && package-lock.json ]];then
-        if ! ncu -e2;then
-          ncu -u
-        fi
-        rm package-lock.json
-        if [[ -L node_modules ]];then
-          unlink node_modules
-        fi
-        npm --package-lock-only i
-      fi
-    '';
-    update-python = ''
-      ${pathAdd [ pur coreutils dasel poetry ]}
-      [[ -e requirements.txt ]] && pur -zfr requirements.txt || true
-      [[ -e requirements.dev.txt ]] && pur -zfr requirements.dev.txt || true
-      if [[ -e pyproject.toml && -e poetry.lock ]];then
-        dasel -f pyproject.toml .tool.poetry.dependencies |
-        cut -d' ' -f1 |
-        grep -v python |
-        sed 's/$/:latest/' |
-        xargs poetry add --lock
-      fi
-    '';
-    update-flake = ''
-      ${pathAdd [ git nix-wrapped ]}
-      if [[ -e flake.nix && -e flake.lock ]];then
-        git status &> /dev/null
-        nix flake update
-        git status &> /dev/null
-      fi
-    '';
-  };
-  pkg = override (build { path = ./.; }) {
-    name = "nle";
-    passthru = { inherit build; };
-  };
 in
-pkg // {
-  inherit scripts;
-  tests = {
-    default = nle.build { path = writeTextDir "meme" ''meme''; };
-  } // scripts;
-}
+(build { path = ./.; }).overrideAttrs (_: {
+  name = "nle";
+  passthru = { inherit build; };
+  passthru.test.default = nle.build { path = writeTextDir "meme" ''meme''; };
+})
