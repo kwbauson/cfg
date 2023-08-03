6c5e8ddb6b7cb899d0c64bb6d309a86bdd7ccbd7
Keith Bauson <kwbauson@gmail.com>
2023-08-03 03:17:54 -0400

    all: update (#1632)

--- pkgs/evilhack.nix
+++ pkgs/evilhack.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 nethack.overrideAttrs (attrs: {
   inherit pname;
-  version = "unstable-2023-08-01";
+  version = "unstable-2023-08-02";
   src = fetchFromGitHub {
     owner = "k21971";
     repo = "EvilHack";
-    rev = "9580bea0b3053cd328935d2a5ef17468bfcecb0f";
-    hash = "sha256-k/EjdPBzndms0Znak2iZ3w7OjRzL6x2nsYrRpocgNCo=";
+    rev = "9bf0cf437a6447d4fb46e2e541f484366a41362d";
+    hash = "sha256-QgJzmdiWtD15OL1ZmnOfeTSoiB7pYFZWCMJJtQj5628=";
   };
   postPatch = ''
     ${attrs.postPatch}
