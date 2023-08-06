19e50d831859d4744a05778ee4d6caaab54b3647
Keith Bauson <kwbauson@gmail.com>
2023-08-06 17:11:36 -0400

    all: update

--- pkgs/nix-index-database.nix
+++ pkgs/nix-index-database.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2023-07-30-032440";
+  version = "2023-08-06-033242";
   src = fetchurl {
     url = "https://github.com/nix-community/${attrs.pname}/releases/download/${attrs.version}/index-aarch64-linux";
-    hash = "sha256-tXQEpKLr/9l+wvWxlqBirRR4OubR4dgrWlsj0y7kyz4=";
+    hash = "sha256-nTyhu8X1NyBKbdhtSAmRliIwBF316KG/hhWzwtOagXw=";
   };
   dontUnpack = true;
   installPhase = ''
--- pkgs/ranger_devicons.nix
+++ pkgs/ranger_devicons.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 stdenv.mkDerivation {
   inherit pname;
-  version = "unstable-2023-06-18";
+  version = "unstable-2023-08-04";
   src = fetchFromGitHub {
     owner = "alexanderjeurissen";
     repo = pname;
-    rev = "1b5780117eeebdfcd221ce45823a1ddef8399848";
-    hash = "sha256-MMPbYXlSLwECf/Li4KqYbSmKZ8n8LfTdkOfZKshJ30w=";
+    rev = "de64ab26fb581c00a803381d522c6b3e48b79415";
+    hash = "sha256-6JEhyU08QEkGdRW2L00ynRaoaaR5PaiVUccEUbtTQuU=";
   };
   installPhase = "cp -r . $out";
   passthru.updateScript = unstableGitUpdater { };
