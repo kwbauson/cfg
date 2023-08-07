b313ca44c55219dbedf8f5d85d6494ef316896c1
Keith Bauson <kwbauson@gmail.com>
2023-08-07 11:14:33 -0400

    jitsi-meet: package with buildNpmPackage (#1636)
    * idk if this will ever work
    
    * i can't believe it
    
    * better patch names and timestamp version
    
    * fix install

--- pkgs/jitsi-meet/default.nix
+++ pkgs/jitsi-meet/default.nix
@@ -1,47 +1,30 @@
 scope: with scope;
 let
+  patched-nixpkgs = applyPatches { src = pkgs.path; patches = [ ./prefetch-npm-deps-ignore-bad.patch ]; };
+  patched-pkgs = import patched-nixpkgs { inherit system; config = { }; overlays = [ ]; };
+in
+patched-pkgs.buildNpmPackage {
+  inherit pname;
+  version = "unstable-2023-02-10";
   src = fetchFromGitHub {
     owner = "jitsi";
     repo = pname;
     rev = "e1ac000cd1f15642218e80ded98ee19188cf2b17";
     hash = "sha256-7wKpUYm6KxNy4W8i4Hcctw6jSiV0+gbz0FnuEcqmjpM=";
   };
-  node_modules = mkYarnModulesWithRebuild {
-    packageJSON = "${src}/package.json";
-    yarnLock = ./yarn.lock;
-    extraNativeBuildInputs = [ pango ];
-    preRebuild = "export RN_WEBRTC_SKIP_DOWNLOAD=1";
-  };
-  jitsi-meet-source-package = stdenv.mkDerivation {
-    name = "jitsi-meet.tar.bz2";
-    inherit src;
-    patches = [ ./changes.patch ];
-    NODE_OPTIONS = [ "--openssl-legacy-provider" "--max_old_space_size=8192" ];
-    buildPhase = ''
-      cp -r ${node_modules} node_modules
-      ./node_modules/.bin/webpack
-      make deploy source-package
-    '';
-    installPhase = ''
-      mv jitsi-meet.tar.bz2 $out
-    '';
-  };
-in
-prev.overrideAttrs (_: {
-  src = jitsi-meet-source-package;
+  npmDepsHash = "sha256-vMTShIpGjubcEgGqMZM9zqoUaAhV/dB8Xh9EH+gB2b8=";
+  makeCacheWritable = true;
+  patches = [ ./jitsi-meet-changes.patch ];
+  nativeBuildInputs = [ python3 pkg-config ];
+  buildInputs = [ pango ];
+  RN_WEBRTC_SKIP_DOWNLOAD = "1";
+  buildPhase = ''
+    make compile deploy source-package
+  '';
+  installPhase = ''
+    tar xf jitsi-meet.tar.bz2
+    mv jitsi-meet $out
+  '';
   meta.platforms = platforms.linux;
-  passthru = { inherit jitsi-meet-source-package; };
-  passthru.jitsi-src = src;
-  passthru.updateScript = [ ];
-  # passthru.updateScript = writeBashBin "update-jitsi" ''
-  #   ${ pathAdd [ yarn coreutils ] }
-  #   set -eo pipefail
-  #   dir=$(mktemp -d /tmp/update-jitsi.XXXXX)
-  #   cd "$dir"
-  #   cp --no-preserve=mode ${src.jitsi-meet}/package{,-lock}.json .
-  #   yarn import --ignore-engines
-  #   cd -
-  #   cp "$dir"/yarn.lock pkgs/jitsi-meet
-  #   rm -r "$dir"
-  # '';
-})
+  passthru.updateScript = unstableGitUpdater { };
+}
