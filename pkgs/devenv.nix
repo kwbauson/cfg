49712ad6f3088973e047c839f01e63d2ff2a6f63
Keith Bauson <kwbauson@gmail.com>
2023-08-16 14:51:57 -0400

    all: update

--- pkgs/devenv.nix
+++ pkgs/devenv.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2023-08-15";
+  version = "unstable-2023-08-16";
   src = fetchFromGitHub {
     owner = "cachix";
     repo = pname;
-    rev = "5c258eec1f7497aa9ec0b8aa85931dcde6925a9f";
-    hash = "sha256-uQKYKEKtdJJzJl1wn/hmUrNjFe6HMETF1GUlKcCOHZw=";
+    rev = "eee80243720b7f284128873a9694a520d9967b2f";
+    hash = "sha256-9SF/H8oCWv166q5o+JtV7tK+koydgFMu02HCB27UWpU=";
   };
   package = (import attrs.src).packages.${system}.default;
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/evilhack.nix
+++ pkgs/evilhack.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 nethack.overrideAttrs (attrs: {
   inherit pname;
-  version = "unstable-2023-08-15";
+  version = "unstable-2023-08-16";
   src = fetchFromGitHub {
     owner = "k21971";
     repo = "EvilHack";
-    rev = "7253753eb1c3242a05e21c5ba8bd9590c8859b94";
-    hash = "sha256-e+9Qarrt/nL36o9RrFdlmtCc7he17LYkwmio/LH7fYU=";
+    rev = "738d7b1fcc8439532de495320e8aeec6771cd096";
+    hash = "sha256-sWuxRG7tXdvnHImM8C6C81c3j5XjdFW9KZ9kVaI2Ox0=";
   };
   postPatch = ''
     ${attrs.postPatch}
