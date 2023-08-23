7efb9b11c74dc5856401e9c0aeaad9bda44b379b
Keith Bauson <kwbauson@gmail.com>
2023-08-22 22:52:26 -0400

    term: fix from kitty and stop using tmux

--- pkgs/term.nix
+++ pkgs/term.nix
@@ -2,14 +2,21 @@ scope: with scope;
 let
   init-file = writeText "init-file" ''
     [[ -e ~/.bash_profile ]] && . ~/.bash_profile
-    ${pathAdd tmux}
-    PROMPT_COMMAND="$PROMPT_COMMAND; trap 'history -a; tmux new -d \"\$BASH_COMMAND\"; exit' DEBUG"
+    one_command() {
+      if [[ $BASH_COMMAND != *_prompt_command* ]];then
+        history -a
+        nohup $BASH_COMMAND > /dev/null &
+        disown
+        exit
+      fi
+    }
+    PROMPT_COMMAND="$PROMPT_COMMAND; trap one_command DEBUG"
   '';
   term = writeBashBin "term" ''
     exec ${kitty}/bin/kitty --single-instance "$@"
   '';
   termbar = writeBashBin "termbar" ''
-    exec ${kitty}/bin/kitty --name termbar bash --init-file ${init-file}
+    exec ${kitty}/bin/kitty --single-instance --name termbar bash --init-file ${init-file}
   '';
 in
 buildEnv { name = pname; paths = [ term termbar ]; }
