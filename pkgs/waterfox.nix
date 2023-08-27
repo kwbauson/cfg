73ba88d47b95ddcde0ad6b975cc3910afa0f2ef8
Keith Bauson <kwbauson@gmail.com>
2023-08-27 07:12:33 +0000

    all: update (#1667)

--- pkgs/waterfox.nix
+++ pkgs/waterfox.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "G5.1.11";
+  version = "G5.1.12";
   src = fetchurl {
     url = "https://cdn1.waterfox.net/${attrs.pname}/releases/${attrs.version}/Linux_x86_64/${attrs.pname}-${attrs.version}.tar.bz2";
-    hash = "sha256-WnO9XFwxTT0yaSFlmgmZMv52+rP9+UJKlXrVCzXreQc=";
+    hash = "sha256-Czy4KuB0XMaRNgf4tzLN/tLWj20b8k51ZQjaHuRX2TA=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = firefox-unwrapped.buildInputs ++ [ gtk2 ];
