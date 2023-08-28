398b80bf7cc21d475fdcf1afaea8f1e228a4d945
Keith Bauson <kwbauson@gmail.com>
2023-08-28 10:01:01 -0400

    fixes

--- modules/nixos/scribblers.nix
+++ modules/nixos/scribblers.nix
@@ -1,21 +1,20 @@
 { config, scope, ... }: with scope;
-let cfg = config.services.scribble; in
+let cfg = config.services.scribblers; in
 {
-  options.services.scribble = {
+  options.services.scribblers = {
     enable = mkEnableOption "scribblers";
-    package = scribblers;
     port = mkOption { type = types.port; default = constants.scribblers.port; };
     address = mkOption { type = types.str; default = "0.0.0.0"; };
     rootPath = mkOption { type = types.str; default = ""; };
   };
   config = mkIf cfg.enable {
-    systemd.services.scribble = {
+    systemd.services.scribblers = {
       wantedBy = [ "multi-user.target" ];
       script = ''
-        export PORT=${cfg.port}
+        export PORT=${toString cfg.port}
         export NETWORK_ADDRESS=${cfg.address}
         export ROOT_PATH=${cfg.rootPath}
-        exec ${cfg.package}/bin/scribblers
+        exec ${scribblers}/bin/scribblers
       '';
     };
   };
