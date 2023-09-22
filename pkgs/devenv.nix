23ff3a29fb1be13917e5d90a353f3da976f2cb5a
Keith Bauson <kwbauson@gmail.com>
2023-09-22 03:12:32 -0400

    all: update

--- pkgs/devenv.nix
+++ pkgs/devenv.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2023-09-20";
+  version = "unstable-2023-09-22";
   src = fetchFromGitHub {
     owner = "cachix";
     repo = pname;
-    rev = "d3b37c5d6fce8919b9a86c4a3bcacd2d0534341a";
-    hash = "sha256-4C+HSXd69qQe12m8Pc6PgLmJGDKURcqkhVdwRu0xrBw=";
+    rev = "1f21193496c515e55c556176e6aa13db9fdad1b4";
+    hash = "sha256-VsbuaBJ7Hx3aqdPPRT2Gp+TmAvtJtcffQGChws9VHZM=";
   };
   package = (import attrs.src).packages.${system}.default;
   passthru.updateScript = unstableGitUpdater { };
