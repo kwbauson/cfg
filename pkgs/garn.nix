af61c3f770af0770ab85ad3e7bff20f30efd8124
Keith Bauson <kwbauson@gmail.com>
2023-11-04 03:13:05 -0400

    all: update (#1729)

--- pkgs/garn.nix
+++ pkgs/garn.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2023-11-03";
+  version = "unstable-2023-11-04";
   src = fetchFromGitHub {
     owner = "garnix-io";
     repo = pname;
-    rev = "41703e980a1b3f60748dc6202660f7241cf31a4e";
-    hash = "sha256-mLbxctUD5qMoSoIwCICXG/JUbGmSUkLkRmz5Ypqelho=";
+    rev = "29c9a64ba080281734a80198e27db6ec97239b83";
+    hash = "sha256-++a9FFeMEK5W7iQTkOAdrM7tP9sD2WRWkKv9k/3xIKk=";
   };
   package = (haskellPackages.callPackage "${attrs.src}/garn.nix" { }).overrideAttrs (old: {
     doCheck = false;
