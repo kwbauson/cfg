c8a592fc714c9065054800f9aed665601b3f55b6
Keith Bauson <kwbauson@gmail.com>
2023-11-16 02:11:29 -0500

    all: update

--- pkgs/garn.nix
+++ pkgs/garn.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2023-11-13";
+  version = "unstable-2023-11-15";
   src = fetchFromGitHub {
     owner = "garnix-io";
     repo = pname;
-    rev = "f075a8a1a2d463ca7b135c25da3c62a7c5c51e86";
-    hash = "sha256-lzJWdhG+x5RkMJ3aaMmNV7g79Sku2jytNDX+dCsxHC8=";
+    rev = "8960624339faa8701a03288692d1dfc4814dc0ae";
+    hash = "sha256-T/wBaOKdz0IKnYf0DgpkX/lnkKJHk8n78byJZUnvZsg=";
   };
   package = compatGetFlakeDefault attrs.src;
   passthru.updateScript = unstableGitUpdater { };
