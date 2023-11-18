def46c81d1b32e84be8c575d8c1a30fcd0973813
Keith Bauson <kwbauson@gmail.com>
2023-11-18 11:21:47 -0500

    scribblers: track brandon's branch in his fork

--- pkgs/scribblers/default.nix
+++ pkgs/scribblers/default.nix
@@ -1,18 +1,18 @@
 scope: with scope;
 buildGoModule {
   inherit pname;
-  version = "unstable-2023-08-29";
+  version = "unstable-2023-11-18";
   src = fetchFromGitHub {
-    owner = "scribble-rs";
+    owner = "dataisbaye";
     repo = "scribble.rs";
-    rev = "b4ac83145eae1bfb674ccb9112312cc77ff0adaa";
-    hash = "sha256-i4XGllrIJODi/Vyo2nK5UjJfU/2dDt2wjzAjqRLCse4=";
+    rev = "8aba83bcb8cf2bc0a6a3d2b517ff8f4781f98b39";
+    hash = "sha256-jnUsIrzoucvW8UyNy0D3lWKgGvaieiUiW+aJnOENdkM=";
   };
-  vendorHash = "sha256-xdadV4WPY7cmsXf+nyPsFCXNTSxLhkau8GZtLopDcuY=";
+  vendorHash = "sha256-8v1EHgBeme5PQ98b9uJoq3t7XWMu12ooF9LmnWDBiDc=";
   patches = [ ./choose-ten.patch ];
   doCheck = false;
-  # passthru.updateScript = _experimental-update-script-combinators.sequence [
-  #   (unstableGitUpdater { })
-  #   (nix-update-script { extraArgs = [ "--flake" "--version" "skip" ]; })
-  # ];
+  passthru.updateScript = _experimental-update-script-combinators.sequence [
+    (unstableGitUpdater { branch = "add-gifs"; })
+    (nix-update-script { extraArgs = [ "--flake" "--version" "skip" ]; })
+  ];
 }
