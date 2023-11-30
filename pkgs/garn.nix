095846fa4218dfc9597912c95bfc659e90e03e67
Keith Bauson <kwbauson@gmail.com>
2023-11-30 11:21:55 -0500

    all: update (#1752)

--- pkgs/garn.nix
+++ pkgs/garn.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2023-11-29";
+  version = "unstable-2023-11-30";
   src = fetchFromGitHub {
     owner = "garnix-io";
     repo = pname;
-    rev = "f5843001012ef790dc3bdb2e0ce9cd5d331cfefe";
-    hash = "sha256-Czzkl5cNf6YZWqMQjwF9RIS1mLh3+nONp7H55+hGCqM=";
+    rev = "b57cc04435fbe6bc91ac343147986cafa2d45db7";
+    hash = "sha256-VpKioSS2lL4//Yk9QHNre865VpwOBmIH7XPdUVe0O4I=";
   };
   package = compatGetFlakeDefault attrs.src;
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/nimlangserver.nix
+++ pkgs/nimlangserver.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 buildNimblePackage {
   inherit pname;
-  version = "unstable-2023-11-20";
+  version = "unstable-2023-11-30";
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
-    rev = "c28a4b52f0011d6cd4f1b8ffd0c5c53766bd13f0";
-    hash = "sha256-vIaTN+duQpDmoMDrelJmK5Wno3M9p3WAfbsDk+wTQ1k=";
+    rev = "c8c684491c5fe78a6481ab3537aa3542fa096c4e";
+    hash = "sha256-NCZxMqKzMIqkrMihDtHVMQ0yfGfrPgENlIhPK3HerQ4=";
   };
   allOverrides.doCheck = false;
   passthru.updateScript = unstableGitUpdater { };
