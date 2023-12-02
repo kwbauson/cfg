2959ee2485dbcb9167efadfe9d1d2d45b52d3eb4
Keith Bauson <kwbauson@gmail.com>
2023-12-02 02:11:31 -0500

    all: update (#1754)

--- pkgs/olivetin.nix
+++ pkgs/olivetin.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2023.10.25";
+  version = "2023.12.1";
   src = fetchurl {
     url = "https://github.com/OliveTin/OliveTin/releases/download/${attrs.version}/OliveTin-linux-amd64.tar.gz";
-    hash = "sha256-kIlZlBIdv8eSImP2pG+1ox/H9k+2rWqmlQnzuG1T31M=";
+    hash = "sha256-paCzQu6599kTvefj/2tB7bG0hjwMYffGgCRhGQvcvsA=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   installPhase = "cp -r . $out";
