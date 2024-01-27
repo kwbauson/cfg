4be6e8b725145359801b7fa48af247dd1bf10e56
Keith Bauson <kwbauson@gmail.com>
2024-01-27 10:34:25 -0500

    all: update (#1796)

--- pkgs/garn.nix
+++ pkgs/garn.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "unstable-2024-01-24";
+  version = "unstable-2024-01-26";
   src = fetchFromGitHub {
     owner = "garnix-io";
     repo = pname;
-    rev = "ca8a2addaf309b85c9a0a72d22aea059fc2bc449";
-    hash = "sha256-3htyxD1V1aoH7PL3+YaAIeXRKWb5h+0uH7q+/9EzjV8=";
+    rev = "a892794e7b41b75e2a899a36ea4f619c0e5ad026";
+    hash = "sha256-GV2xL+veABI/H+fWt6Gc9qlOx5fG1u8ELRSffLap2+4=";
   };
   package = compatGetFlakeDefault attrs.src;
   passthru.updateScript = unstableGitUpdater { };
