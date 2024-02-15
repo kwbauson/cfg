685db05d38fc27f63fdff975c0644140afae29ca
Keith Bauson <kwbauson@gmail.com>
2024-02-15 02:46:10 -0500

    all: update (#1813)

--- pkgs/typos-lsp.nix
+++ pkgs/typos-lsp.nix
@@ -1,14 +1,14 @@
 scope: with scope;
 rustPlatform.buildRustPackage {
   inherit pname;
-  version = "unstable-2024-02-05";
+  version = "unstable-2024-02-11";
   src = fetchFromGitHub {
     owner = "tekumara";
     repo = "typos-vscode";
-    rev = "68f1d4ceda255e74ccdb6b6bb941262e1b050e16";
-    hash = "sha256-LzemgHVCuLkLaJyyrJhIsOOn+OnYuiJsMSxITNz6R8g=";
+    rev = "df666b3e934e61eba848df5bacd26103a8731396";
+    hash = "sha256-B+LDz6saxvfOwyjl9wsPo8pJZI0T7KQsUmkN0YjfhmI=";
   };
-  cargoHash = "sha256-f7tUgi+0WgbJdaO/vF0H8cC0o3aTAI2kN0q0yNDKZdc=";
+  cargoHash = "sha256-DcfKaTv2X/R9m7E74ftBUBAmuvUyBiiPXUMQBgKFCz8=";
   meta.mainProgram = pname;
   passthru.updateScript = _experimental-update-script-combinators.sequence [
     (unstableGitUpdater { })
