8a96b9df4364d2aef8c73a37adab5e52156d6d31
Keith Bauson <kwbauson@gmail.com>
2024-04-19 21:18:50 -0400

    all: update (#1879)

--- pkgs/olivetin.nix
+++ pkgs/olivetin.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2024.04.18";
+  version = "2024.04.20";
   src = fetchurl {
     url = "https://github.com/OliveTin/OliveTin/releases/download/${attrs.version}/OliveTin-linux-amd64.tar.gz";
-    hash = "sha256-CjleQTDXCkoLBuxcXuxet2e38zCu8Uqbrr//g7Fm4LA=";
+    hash = "sha256-P8WvSC37QMdyy14YCWosiSfHgEAlYfnrvdBWg9/uWao=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   installPhase = "cp -r . $out";
