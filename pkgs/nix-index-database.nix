04b4aa1c90e7d4631960ba29744c71078bcc7848
Keith Bauson <kwbauson@gmail.com>
2024-04-28 03:14:35 -0400

    all: update (#1888)

--- pkgs/nix-index-database.nix
+++ pkgs/nix-index-database.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2024-04-21-030753";
+  version = "2024-04-28-030722";
   src = fetchurl {
     url = "https://github.com/nix-community/${attrs.pname}/releases/download/${attrs.version}/index-aarch64-linux";
-    hash = "sha256-WSE5mVWUhYl4ZnNFucSc6AdbM6cIhlpUB7LUqG0IU8o=";
+    hash = "sha256-S6bSkrd+N1VJvLz9uZX3cgnm6TF88F9dgqllXNmaCco=";
   };
   dontUnpack = true;
   installPhase = ''
