4291ad0415edc37aedaf06edc295e232e8b93007
Keith Bauson <kwbauson@gmail.com>
2024-05-19 03:12:33 -0400

    all: update

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2024-05-18";
+  version = "0-unstable-2024-05-19";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "11d780d87a22fe6c2734ee6da7a4fcfaf3f90508";
-    hash = "sha256-Iyd/5sCUt/DZbzue8xrRGmKmSTVsj7MQu2XcOnLJK5E=";
+    rev = "e9396c8eb5fef89530a63511fdb1848404a4b0f3";
+    hash = "sha256-FhJRe9P0/lIc5Ol5ZD7aAoaZLevWlWTGRcwQjJVjaQM=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/nix-index-database.nix
+++ pkgs/nix-index-database.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2024-05-12-030850";
+  version = "2024-05-19-030652";
   src = fetchurl {
     url = "https://github.com/nix-community/${attrs.pname}/releases/download/${attrs.version}/index-aarch64-linux";
-    hash = "sha256-5RmPopUBaz46J/pcV39NGaQ+qvnWUqJwnrNBYGUlJ64=";
+    hash = "sha256-gyIH57nii/tXiNxK+45zUr/g7fLSpHzH8OM+uygRcfI=";
   };
   dontUnpack = true;
   installPhase = ''
