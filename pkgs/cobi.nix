37f907d8d8f205cd7d6e4a94a66e9483e3dc593f
Keith Bauson <kwbauson@gmail.com>
2024-09-03 03:15:08 -0400

    all: update

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2024-08-31";
+  version = "0-unstable-2024-09-03";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "a5f68d6ca55e55a6733fe3b8afef5e8ea087740f";
-    hash = "sha256-WXPQTXK+5BpHk12LYZeRXYovsdItDZg40cNRiX1cGN4=";
+    rev = "4ebeda0afda256e059148496851455ca8c6045d2";
+    hash = "sha256-XjAobsMrj6l/z/m36Gv0SlPzVIFwxzhOX4uTGHl2cBs=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/olivetin.nix
+++ pkgs/olivetin.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2024.08.31";
+  version = "2024.09.02";
   src = fetchurl {
     url = "https://github.com/OliveTin/OliveTin/releases/download/${attrs.version}/OliveTin-linux-amd64.tar.gz";
-    hash = "sha256-1K3JnE5i96mPBoEmjrv2umFszLcnmQ4EvFN8eWQcQRg=";
+    hash = "sha256-muo8wBqm9PFGa2kn1FBJJ6SDAq58rDC9ZTB602kBTaI=";
   };
   nativeBuildInputs = [ autoPatchelfHook ];
   installPhase = "cp -r . $out";
