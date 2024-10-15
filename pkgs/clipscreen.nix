7b24268a599fcd0200670ae9adc5d9a83b05c56c
Keith Bauson <kwbauson@gmail.com>
2024-10-14 23:54:34 -0400

    init clipscreen

---
+++ pkgs/clipscreen.nix
@@ -0,0 +1,18 @@
+scope: with scope;
+stdenv.mkDerivation {
+  inherit pname;
+  version = "0-unstable-2024-10-14";
+  src = fetchFromGitHub {
+    owner = "splitbrain";
+    repo = pname;
+    rev = "530215c37db1f23ea8e67695fdc052a597d08ab0";
+    hash = "sha256-/NRJXlnQcO5lkWPnupAV8+yYrV1db2vL/mZcR0fYL54=";
+  };
+  buildInputs = with xorg; [ pkg-config cairo libXfixes libXrandr libXcomposite ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp clipscreen $out/bin
+  '';
+  meta.mainProgram = pname;
+  passthru.updateScript = unstableGitUpdater { };
+}
