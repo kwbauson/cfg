7d6112eec549540e693b7ba35d17acb0bf301ea6
Keith Bauson <kwbauson@gmail.com>
2024-11-11 21:50:34 -0500

    update scribblers

--- pkgs/scribblers.nix
+++ pkgs/scribblers.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 buildGoModule {
   inherit pname;
-  version = "0-unstable-2024-01-28";
+  version = "0-unstable-2024-11-11";
   src = fetchFromGitHub {
     owner = "kwbauson";
     repo = "scribble.rs";
-    rev = "3597bb7147e54fa6c8dd9ff05df02a23dfac7aca";
-    hash = "sha256-IKMZBYgtbaToQ87Ivtvs3KHtlv0UC+3xVs6rHoi+p64=";
+    rev = "c8d617c8767670668763367325ceb03ae9007225";
+    hash = "sha256-K4hRoCt2TGgF3+1V/f+sDHlz7vgosHAZWsJkQ4xyou4=";
   };
   vendorHash = "sha256-xdadV4WPY7cmsXf+nyPsFCXNTSxLhkau8GZtLopDcuY=";
   doCheck = false;
