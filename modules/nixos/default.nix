1c7dc292fa314ecdf336c2efee2c0552a1729cd3
Keith Bauson <kwbauson@gmail.com>
2024-12-12 22:17:07 -0500

    enable online check

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -28,7 +28,6 @@
   networking.networkmanager.wifi.powersave = mkDefault false;
   networking.hostName = mkDefault machine-name;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
-  systemd.services.NetworkManager-wait-online.enable = mkDefault false;
 
   hardware.enableRedistributableFirmware = true;
   hardware.enableAllFirmware = true;
