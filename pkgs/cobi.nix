20ea5a0e974027ba6254a996d89e0679520980ff
Keith Bauson <kwbauson@gmail.com>
2024-12-13 20:48:51 -0500

    all: update (#2043)

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2024-12-13";
+  version = "0-unstable-2024-12-14";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "da28259a9a67b4330d77e8959a17618301897d9a";
-    hash = "sha256-5KeY8K4xlWS92ztkvoXsBkaFpSTfI5bjb91wLkag2sU=";
+    rev = "2abf5df29c81017508acec152d75e2b542ca327e";
+    hash = "sha256-TEP/Lkv1o45ViVryFZ7oaSd4HeheC+nmfR8bV1Rono0=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
