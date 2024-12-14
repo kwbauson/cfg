011af11b8fa1241e5b3d95c7d34831b11da344eb
Keith Bauson <kwbauson@gmail.com>
2024-12-13 20:45:19 -0500

    all: update

--- pkgs/nimlangserver.nix
+++ pkgs/nimlangserver.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 buildNimblePackage {
   inherit pname;
-  version = "1.6.0-unstable-2024-12-11";
+  version = "1.6.0-unstable-2024-12-13";
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
-    rev = "cc2ef410548bab416304fada49c723bd264044c7";
-    hash = "sha256-7sUJBjNMHqY94tZ+XfJfaWdD5HxPOAYkR4nSR1e7RiU=";
+    rev = "db19b532ec47011c018d1e136bad703e69643538";
+    hash = "sha256-4nR/9Ztm42U8RkvKKfWdkNWVjJTRY7bV4p45K4gBBLM=";
   };
   passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
   meta.skipBuild = true;
