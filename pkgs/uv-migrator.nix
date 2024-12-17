962f9333da2a6e164ddc6d0cd8078185974a8d47
Keith Bauson <kwbauson@gmail.com>
2024-12-17 13:09:12 -0500

    uv-migrator (#2047)
    * trying to package litellm
    
    * build old uv
    
    * meh i give up
    
    * remove uv2nix input
    
    * no migrator on darwin
    
    * builds on darwin too

---
+++ pkgs/uv-migrator.nix
@@ -0,0 +1,21 @@
+scope: with scope;
+rustPlatform.buildRustPackage {
+  inherit pname;
+  version = "2025.1.0-unstable-2024-12-14";
+  src = fetchFromGitHub {
+    owner = "stvnksslr";
+    repo = pname;
+    rev = "88e26d82fc9244a76464dd355bfd31826f0069e6";
+    hash = "sha256-sVhNF4bL/zsZvXX+JnHOwkKvBHmzy+iFFRBLBl6zfu8=";
+  };
+  cargoHash = "sha256-va/qLJdD+NGLz8oQRUsjlo7okbaaAqla3Jn5FabzO6M=";
+  nativeBuildInputs = [ makeWrapper ];
+  postInstall = ''
+    wrapProgram $out/bin/uv-migrator --prefix PATH : ${makeBinPath [ uv_050 ]}
+  '';
+  meta.mainProgram = pname;
+  passthru.updateScript = _experimental-update-script-combinators.sequence [
+    (unstableGitUpdater { tagPrefix = "v"; })
+    (nix-update-script { extraArgs = [ "--flake" "--version" "skip" ]; })
+  ];
+}
---
+++ pkgs/uv_050.nix
@@ -0,0 +1,23 @@
+scope: with scope;
+rustPlatform.buildRustPackage rec {
+  pname = "uv";
+  version = "0.5.0";
+  src = fetchFromGitHub {
+    owner = "astral-sh";
+    repo = "uv";
+    rev = version;
+    hash = "sha256-CTMEjqV3Cn/Zt91OpNwF3bTLoTD/TPeRsGvLohI15Ng=";
+  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-dtbyjVKBipxqeRYddjCfRXx6wgVXzblow2PY4JufKKw=";
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+  buildInputs = [ bash ];
+  postPatch = ''
+    sed -i '/let attempts = \[".bin.sh"/s@"/bin/sh"@"${bash}/bin/sh"@' crates/uv-python/src/libc.rs
+  '';
+  doCheck = false;
+  meta.mainProgram = "uv";
+}
