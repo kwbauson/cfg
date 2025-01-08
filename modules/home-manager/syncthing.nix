7d68aaf7ea5e99225e5f54e63e4193a854dd93d4
Keith Bauson <kwbauson@gmail.com>
2025-01-08 16:53:09 -0500

    mmm

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -1,6 +1,10 @@
 { scope, ... }: with scope;
 let
   devices = [ "keith-desktop" "keith-xps" ];
+  mkDevices = mapAttrs (name: value: {
+    addresses = [ "tcp://${name}" ];
+    autoAcceptFolders = true;
+  } // value);
   mkDevice = id: {
     inherit id;
     allowedNetworks = "100.0.0.0/8";
@@ -17,7 +21,7 @@ in
         globalAnnounceEnabled = false;
         urAccepted = -1;
       };
-      devices = {
+      devices = mkDevices {
         keith-desktop = mkDevice "WLPCWRZ-SIRCUKK-2B2F6P6-XA5O7HI-DXZFMXI-UB4FAE4-6NPYQ7E-HPHSJAG";
         keith-xps = mkDevice "TLPHMKL-SMTBB6W-OODSBGW-DXQ36UB-ILKK67U-2ZKPIKI-G4TZNSP-UMWXNAH";
       };
