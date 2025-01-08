72e47f46ed046195e3cb5b9ad4b0a1cc9735b8ed
Keith Bauson <kwbauson@gmail.com>
2025-01-08 17:49:48 -0500

    try nixos module

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -1,20 +1,21 @@
-{ scope, machine-name, ... }: with scope;
+{ config, username, scope, machine-name, ... }: with scope;
 let
   devices = [ "keith-desktop" "keith-xps" ];
   mkDevices = mapAttrs (name: value: {
-    addresses = [ "tcp://${name}" ];
+    addresses = [ "tcp://${constants.${name}.ip}" ];
     autoAcceptFolders = true;
   } // value);
   mkDevice = id: {
     inherit id;
-    allowedNetworks = "100.0.0.0/8";
+    # allowedNetworks = "100.0.0.0/8";
   };
   mkFolder = arg: if isString arg then { path = arg; inherit devices; } else arg;
+  inherit (config.users.users.${username}) home;
 in
 {
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
   services.syncthing = {
-    enable = true;
-    extraOptions = [ "--no-default-folder" ];
+    enable = elem machine-name (attrNames config.services.syncthing.settings.devices);
     settings = {
       options = {
         listenAddresses = [ "tcp://${constants.${machine-name}.ip}:22000" ];
@@ -27,7 +28,7 @@ in
         keith-xps = mkDevice "TLPHMKL-SMTBB6W-OODSBGW-DXQ36UB-ILKK67U-2ZKPIKI-G4TZNSP-UMWXNAH";
       };
       folders = {
-        notes = mkFolder "~/notes";
+        notes = mkFolder "${home}/notes";
       };
     };
   };
