cb4296bd2f56e97e74a2cbdb7b2574489f892e94
Keith Bauson <kwbauson@gmail.com>
2025-01-08 13:41:31 -0500

    remmina broken on darwin

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -46,6 +46,6 @@
     inherit i3-easyfocus iproute2 iputils pavucontrol strace dzen2
       maim zoom-us acpi usbutils xdotool qtbr signal-desktop discord zathura;
     inherit breeze-icons nixos-install-tools arduino util-linux steamtinkerlaunch gnutar;
-    inherit man-pages ncdu bitwarden-cli networkmanagerapplet;
+    inherit man-pages ncdu bitwarden-cli remmina networkmanagerapplet;
   };
 }
