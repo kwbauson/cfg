ef2173f6d6bcf299c1ed900500f120e7cfc7b9fd
Keith Bauson <kwbauson@gmail.com>
2025-01-08 16:06:32 -0500

    syncthing config

---
+++ modules/home-manager/syncthing.nix
@@ -0,0 +1,24 @@
+{ scope, ... }: with scope;
+let
+  devices = [ "keith-desktop" ];
+  mkDevice = id: {
+    inherit id;
+    allowedNetworks = "100.0.0.0/8";
+  };
+  mkFolder = arg: if isString arg then { path = arg; inherit devices; } else arg;
+in
+{
+  services.syncthing = {
+    enable = true;
+    extraOptions = [ "--no-default-folder" ];
+    settings = {
+      devices = {
+        keith-desktop = mkDevice "WLPCWRZ-SIRCUKK-2B2F6P6-XA5O7HI-DXZFMXI-UB4FAE4-6NPYQ7E-HPHSJAG";
+      };
+      folders = {
+        ssh_config_d = mkFolder "~/.ssh/config.d";
+        notes = mkFolder "~/notes";
+      };
+    };
+  };
+}
