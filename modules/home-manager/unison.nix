6785eaf8e45bd50835239d2cdc66a1963372ca07
Keith Bauson <kwbauson@gmail.com>
2025-01-09 12:07:23 -0500

    require network for unison syncs

--- modules/home-manager/unison.nix
+++ modules/home-manager/unison.nix
@@ -2,13 +2,21 @@
 let
   sync-machines = [ "keith-desktop" "keith-xps" ];
 in
-{
-  home.packages = [ unison ];
-  services.unison = {
-    enable = elem machine-name sync-machines;
-    pairs.sync.roots = [
-      "${config.home.homeDirectory}/sync"
-      "ssh://keith-server/sync"
-    ];
-  };
-}
+mkMerge [
+  { home.packages = [ unison ]; }
+  (mkIf (elem machine-name sync-machines) {
+    services.unison = {
+      enable = true;
+      pairs.sync.roots = [
+        "${config.home.homeDirectory}/sync"
+        "ssh://keith-server/sync"
+      ];
+    };
+    systemd.user.services = forAttrs' config.services.unison.pairs (name: _: {
+      name = "unison-pair-${name}";
+      value = {
+        Unit.After = [ "network-online.target" ];
+      };
+    });
+  })
+]
