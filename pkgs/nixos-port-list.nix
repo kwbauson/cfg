bb13557c720ea2a5ce31a478907d70fbf8348ec3
Keith Bauson <kwbauson@gmail.com>
2025-01-12 13:52:16 -0500

    nixos-port-list: more generic and include home-manager services

--- pkgs/nixos-port-list.nix
+++ pkgs/nixos-port-list.nix
@@ -6,19 +6,32 @@ scope: with scope; (
   ''
 ).overrideAttrs (_: {
   passthru.get = flip pipe [
-    (config: config.services)
-    (filterAttrs (name: attrs: all id
-      [
-        # exclude deprecations that error
-        (!elem name [ "redis" ])
-        # only enabled
-        ((tryEval attrs.enable or false).value)
-      ]))
-    (mapAttrs (_: filterAttrs (n: _: match "^port$|.+Port$" n != null)))
-    (filterAttrs (_: attrs: attrs != { }))
-    (mapAttrsToList (s: mapAttrsToList (a: p: nameValuePair "services.${s}.${a}" p)))
-    flatten
-    (concatMapStringsSep "\n" (v: "${v.name} = ${toString v.value};"))
+    (config:
+      let
+        getPath = p: getAttrFromPath (splitString "." p) config;
+        mkGroup = path: mapAttrsToList
+          (name: attrs: { inherit path name attrs; })
+          (getPath path);
+      in
+      flatten [
+        (mkGroup "services")
+        (mkGroup "programs")
+        (mapAttrsToList
+          (user: _: [
+            (mkGroup "home-manager.users.${user}.services")
+            (mkGroup "home-manager.users.${user}.programs")
+          ])
+          config.home-manager.users)
+      ]
+    )
+    # exclude deprecations that error
+    (filter (g: g.path == "services" && !elem g.name [ "redis" ]))
+    (filter (g: (tryEval g.attrs.enable or false).value))
+    # only portish named attrs
+    (map (g: g // { attrs = filterAttrs (n: _: match "^port$|.+Port$" n != null) g.attrs; }))
+    (filter (g: g.attrs != { }))
+    (concatMap (g: mapAttrsToList (n: v: { inherit (g) path name; portAttr = n; portValue = v; }) g.attrs))
+    (concatMapStringsSep "\n" (g: "${g.path}.${g.name}.${g.portAttr} = ${toString g.portValue};"))
     (str: ''
       Note: this list is best-effort
 
