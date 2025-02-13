861f0704e56f906976ccdde7a86432acfe6038b5
Keith Bauson <kwbauson@gmail.com>
2025-02-13 02:15:47 -0500

    all: update

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2025-02-11";
+  version = "0-unstable-2025-02-13";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "b572457758dc0260c0d7fec92534707a93c1dd74";
-    hash = "sha256-/5ibBJ1pLz0XTMqj/Ih4N4/nkC2emO0GPOtA2FO7ZyQ=";
+    rev = "5bb7e93fbfbce3262db8adb51d8dba05fa0873cb";
+    hash = "sha256-vER0X2jbQSSRNVxghOUIdjzHZSF4H/E4EMT79NRJ8ok=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
