adf7f0d95463f0b868284e2e1ce83b87851bddf4
Keith Bauson <kwbauson@gmail.com>
2025-02-20 09:09:21 -0500

    uv-migrator: fix cargo deprecation

--- pkgs/uv-migrator.nix
+++ pkgs/uv-migrator.nix
@@ -8,7 +8,8 @@ rustPlatform.buildRustPackage {
     rev = "f68b29d1d4afc4ba1b1d9a25d355d07759d44997";
     hash = "sha256-E4WoQFeyTK0BzBrDJS/9ob9CCSPzjFkZly6JBtmJBMg=";
   };
-  cargoHash = "sha256-lAbZljyEWEWzW0cjydvnu4OwTVbaukw2YH2gd7aYZJY=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-c3Kqp/WSCR42PmCdBqGGj+G0soSJcGssNscaBtbiDrA=";
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/uv-migrator --prefix PATH : ${makeBinPath [ uv_050 ]}
