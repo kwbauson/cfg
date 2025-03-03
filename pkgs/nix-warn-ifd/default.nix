6636a7a548ad422e0f2ddd9c9bf0efa09877f269
Keith Bauson <kwbauson@gmail.com>
2025-03-03 13:38:49 -0500

    build less and no tests for patched nix

--- pkgs/nix-warn-ifd/default.nix
+++ pkgs/nix-warn-ifd/default.nix
@@ -2,4 +2,9 @@ scope: with scope;
 nix.overrideAttrs (old: {
   patches = old.patches or [ ] ++ [ ./warn-ifd.patch ];
   doCheck = false;
+  passthru = { };
+  outputs = [ "out" ];
+  meta = recursiveUpdate old.meta {
+    outputsToInstall = [ "out" ];
+  };
 })
