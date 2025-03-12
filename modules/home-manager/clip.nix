6ebf5700f807cfc3346b159e4145573bf234558b
Keith Bauson <kwbauson@gmail.com>
2025-03-11 21:42:05 -0400

    remove unimplmented stuff from clip and use osc

--- modules/home-manager/clip.nix
+++ modules/home-manager/clip.nix
@@ -2,15 +2,6 @@
 let cfg = config.services.clip; in
 {
   options.services.clip = {
-    enable = mkEnableOption "clip";
-    port = mkOption {
-      type = types.port;
-      default = 6543;
-    };
-    hosts = mkOption {
-      type = types.listOf types.str;
-      default = [ ];
-    };
     sync-primary.enable = mkEnableOption "sync-primary";
   };
   config = {
@@ -21,14 +12,6 @@ let cfg = config.services.clip; in
       };
       Install = { WantedBy = [ "graphical-session.target" ]; };
       Service.ExecStart = "${getExe clip} --sync_primary";
-    };
-    systemd.user.services.clip-server = mkIf cfg.enable {
-      Unit = {
-        PartOf = [ "graphical-session.target" ];
-        After = [ "graphical-session.target" ];
-      };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-      Service.ExecStart = "${getExe clip} --server --port ${toString cfg.port} ${optionalString (cfg.hosts != []) "--hosts ${concatStringsSep "," cfg.hosts}"}";
     };
   };
 }
