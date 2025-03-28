b30a9b33571fbceef9cfef290f5723c7bf2f4881
Keith Bauson <kwbauson@gmail.com>
2025-03-28 03:17:27 -0400

    all: update

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2025-03-27";
+  version = "0-unstable-2025-03-28";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "83eb90f14b4c3343c7ed98d461e094087504f757";
-    hash = "sha256-mwUbpSD/OxI3fwI8rGsjZAWv8KdE80X7heKWGHh+xjU=";
+    rev = "07f6ed96f59650009f73a96bc228d62547841d53";
+    hash = "sha256-pEodPrnqV4P6CS3vhQ7VDC4Gcb5H5G1RvHQfzMboFdA=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/nimlangserver.nix
+++ pkgs/nimlangserver.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 buildNimblePackage {
   inherit pname;
-  version = "1.10.0-unstable-2025-03-26";
+  version = "1.10.2-unstable-2025-03-27";
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
-    rev = "018f5640dcc9badca5cb466facfee48b1350ca19";
-    hash = "sha256-CbdlDcEkX/pPXEbIsSM6S9INeBCwgjx7NxonjUJAHrk=";
+    rev = "64b093616ebcd2b37b6e2b1a213796c98c8cbdf0";
+    hash = "sha256-xeSfg3AMnvMhsESrRyn/Mm+LubLF320lWPfmTjA3rFE=";
   };
   passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
   meta.skipBuild = true;
