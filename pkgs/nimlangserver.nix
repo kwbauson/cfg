0a4d162ba34e56512f764d5b2cf04ea44d61e16a
Keith Bauson <kwbauson@gmail.com>
2025-04-13 03:16:22 -0400

    all: update

--- pkgs/nimlangserver.nix
+++ pkgs/nimlangserver.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 buildNimblePackage {
   inherit pname;
-  version = "1.10.2-unstable-2025-04-02";
+  version = "1.10.2-unstable-2025-04-11";
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
-    rev = "380e4634f2891a926e40c334161931676b074b5a";
-    hash = "sha256-Tx/2aA5q2K44E2tSlbZNwubJDHV9V+8EDFwR5c0Gjn8=";
+    rev = "43d3023c0f6e1e31b709fa60ed985cb825574dfa";
+    hash = "sha256-He7lJdVUxGWVCZFPLhGWGiuT4HIIkZiAZ/+qi6dDrsA=";
   };
   passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
   meta.skipBuild = true;
--- pkgs/nix-index-database.nix
+++ pkgs/nix-index-database.nix
@@ -1,10 +1,10 @@
 scope: with scope;
 stdenv.mkDerivation (attrs: {
   inherit pname;
-  version = "2025-03-30-032852";
+  version = "2025-04-13-041853";
   src = fetchurl {
     url = "https://github.com/nix-community/${attrs.pname}/releases/download/${attrs.version}/index-aarch64-linux";
-    hash = "sha256-OhXtIiYnvzqoYdkgBYoY7MV1CfHahB3g59co0r7g1zI=";
+    hash = "sha256-xXXx5p2k2B92YRFtsK2r3mm1Vf8Y9uK5Xj4YPULIr8I=";
   };
   dontUnpack = true;
   installPhase = ''
