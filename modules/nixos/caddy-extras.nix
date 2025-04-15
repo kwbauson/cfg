7d14c12032bf492eb2b5d96678f3c7a72af4f691
Keith Bauson <kwbauson@gmail.com>
2025-04-15 19:20:49 -0400

    update zaddy vendorHash

--- modules/nixos/caddy-extras.nix
+++ modules/nixos/caddy-extras.nix
@@ -23,6 +23,7 @@ in
   config.services.caddy = {
     package = cobi.pkgs.zaddy.overrideAttrs {
       postInstall = caddy.postInstall;
+      vendorHash = "sha256-7k5KobsresXiTLL5d9W6Q9yJ9Pi+cTIR1eqsfxL5MoQ=";
     };
     globalConfig = ''
       order authenticate before respond
