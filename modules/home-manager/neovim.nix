abfceaf8e4cf1e702cbf2117b86f9801a740a3b0
Keith Bauson <kwbauson@gmail.com>
2025-04-25 15:26:18 -0400

    switch to nixvim

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,37 +6,8 @@
   ];
 
   programs.nixvim = {
-    # enable = true;
-    defaultEditor = true;
-    nixpkgs.useGlobalPackages = true;
-  };
-
-  included-packages = optionalAttrs (!isMinimal) {
-    inherit nixpkgs-fmt shellcheck vim-language-server lua-language-server;
-    inherit typescript-language-server;
-  };
-  programs.neovim = {
     enable = true;
     defaultEditor = true;
-    ${attrIf (!isMinimal) "extraPackages"} = attrValues { inherit nil lua-language-server; };
-    plugins = with vimPlugins; attrValues {
-      inherit
-        # core
-        nvim-scrollview quick-scope vim-code-dark tcomment_vim
-        vim-better-whitespace vim-lastplace vim-multiple-cursors vim-sensible
-        vim-peekaboo
-        # project stuff
-        vim-startify git-conflict-nvim vim-fugitive vim-airline
-        nvim-web-devicons patched-barbar-nvim
-        # programming
-        formatter-nvim nvim-lspconfig telescope-nvim;
-      nvim-treesitter = nvim-treesitter.withAllGrammars;
-      treesitter-koka = neovimUtils.grammarToPlugin tree-sitter-grammars.tree-sitter-koka;
-      inherit fzf-vim; # FIXME telescope-nvim
-    };
-    extraConfig = "
-      source ${config.home.homeDirectory}/cfg/modules/home-manager/init.vim
-      source ${config.home.homeDirectory}/cfg/modules/home-manager/init.lua
-    ";
+    nixpkgs.useGlobalPackages = true;
   };
 }
--- pkgs/nixvim.nix
+++ pkgs/nixvim.nix
@@ -1,5 +1,5 @@
 scope: with scope;
-importPackage (attrs: {
+importPackage rec {
   inherit pname;
   version = "0-unstable-2025-04-25";
   src = fetchFromGitHub {
@@ -9,17 +9,104 @@ importPackage (attrs: {
     hash = "sha256-Ig9m38EL6gZHwqKS89401Eooe/Zi9CqQsaehVlf1wcQ=";
   };
   passthru.updateScript = unstableGitUpdater { };
-  flake = import attrs.src;
-  package = attrs.flake.legacyPackages.${system}.makeNixvim attrs.configuration;
+  flake = import src;
+  package = flake.legacyPackages.${system}.makeNixvimWithModule {
+    inherit pkgs;
+    module = configuration;
+  };
+
+  lib = flake.lib.nixvim // {
+    mkKeyMaps = mapAttrsToList (
+      key: value: { inherit key; } // (
+        if !isString value then value else {
+          action = value;
+          options.silent = true;
+        }
+      )
+    );
+    mkCmdlineMap = action: fallback: lib.mkRaw /* lua */ ''{
+      function (cmp)
+        if vim.fn.getcmdtype() ~= ':' then
+          return cmp.${action}()
+        end
+      end,
+      "${fallback}",
+    }'';
+  };
 
   configuration = {
-    colorschemes.vscode.enable = true;
+    colorschemes.nightfox.enable = true;
+    colorschemes.nightfox.flavor = "carbonfox";
+    opts = rec {
+      number = true;
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = shiftwidth;
+      softtabstop = shiftwidth;
+      signcolumn = "yes:1";
+      title = true;
+      backup = false;
+      writebackup = false;
+      hidden = true;
+    };
+    keymaps = lib.mkKeyMaps {
+      "<C-p>" = { mode = "c"; action = "<up>"; };
+      "<C-n>" = { mode = "c"; action = "<down>"; };
+      gh = ":BufferLineCyclePrev<cr>";
+      gl = ":BufferLineCycleNext<cr>";
+      gH = ":BufferLineMovePrev<cr>";
+      gL = ":BufferLineMoveNext<cr>";
+      gb = ":BufferLinePick<cr>";
+      " p" = ":Telescope find_files<cr>";
+      " d" = ":bdelete!<cr>";
+    };
+    diagnostics.virtual_lines.current_line = true;
     plugins = {
       bufferline.enable = true;
       bufferline.settings.options.show_buffer_close_icons = false;
       web-devicons.enable = true;
       lualine.enable = true;
       treesitter.enable = true;
+      lsp.enable = true;
+      lsp.servers = {
+        nil_ls.enable = true; # FIXME switch to nixd?
+        nil_ls.settings.formatting.command = [ "nixpkgs-fmt" ];
+        ts_ls.enable = true;
+      };
+      lsp-format.enable = true;
+      gitsigns.enable = true;
+      comment.enable = true;
+      comment.settings.toggler.block = "gcb";
+      telescope.enable = true;
+      nvim-tree.enable = true;
+      scrollview.enable = true;
+      blink-cmp.enable = true;
+      blink-cmp.settings = {
+        signature.enabled = true;
+        completion.documentation.auto_show = true;
+        completion.documentation.auto_show_delay_ms = 50;
+        keymap."<Tab>" = [ "select_and_accept" "fallback" ];
+        keymap."<C-l>" = lib.mkCmdlineMap "select_and_accept" "fallback";
+        keymap."<C-p>" = lib.mkCmdlineMap "select_prev" "fallback_to_mappings";
+        keymap."<C-n>" = lib.mkCmdlineMap "select_next" "fallback_to_mappings";
+        cmdline.keymap.preset = "inherit";
+        cmdline.completion.menu.auto_show = true;
+      };
+      lastplace.enable = true;
+      fugitive.enable = true;
+      startify.enable = true;
+      startify.settings = {
+        fortune_use_unicode = true;
+        session_persistence = true;
+        lists = [
+          { type = "sessions"; header = [ "   Sessions" ]; }
+          { type = "dir"; header = [{ __raw = "'   MRU' .. vim.loop.cwd()"; }]; }
+          { type = "files"; header = [ "   MRU" ]; }
+          { type = "bookmarks"; header = [ "   Bookmarks" ]; }
+          { type = "commands"; header = [ "   Commands" ]; }
+        ];
+      };
+      which-key.enable = true;
     };
   };
-})
+}
