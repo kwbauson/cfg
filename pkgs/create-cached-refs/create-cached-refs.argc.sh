f05797d83d45fda78c00609e67a7ca9f96e5ef52
Keith Bauson <kwbauson@gmail.com>
2025-05-08 11:21:20 -0400

    prevent concurrent updating of cache branch (#2162)
    * try a needs meme
    
    * matrix cache?
    
    * ope
    
    * concurrency group
    
    * what happens on non-branch
    
    * starting outputs
    
    * can i haz outputs
    
    * output path?
    
    * outputs in step
    
    * maybe more outputs
    
    * ope
    
    * try not on branch
    
    * can i use an object for outputs
    
    * this?
    
    * how about this
    
    * maybe
    
    * ope again
    
    * try fix paths
    
    * ope
    
    * blagr
    
    * more verbose
    
    * try fixing realpath
    
    * trigger workflow
    
    * cached-refs on main

--- pkgs/create-cached-refs/create-cached-refs.argc.sh
+++ pkgs/create-cached-refs/create-cached-refs.argc.sh
@@ -7,13 +7,12 @@ set -euo pipefail
 # @option --tag=default Name for generated paths file
 generate() {
   out=$PWD/$argc_out
-  paths=$argc_paths
 
   mkdir -p "$out"/paths
   cp --no-preserve=mode $TEMPLATE_FLAKE "$out"/flake.nix
   cp --no-preserve=mode $TEMPLATE_DEFAULT "$out"/default.nix
 
-  cd "$paths"
+  cd "$argc_paths"
   (
     echo "{ mkPath }:"
     echo "{"
@@ -36,30 +35,26 @@ generate() {
 # @option --email=noreply@example.com
 push() {
   root=$PWD
+  paths=$(realpath "$argc_paths")
+  worktree=/tmp/create-cached-refs/cached
 
-  try_push() {
-    cd "$root"
-    worktree=/tmp/create-cached-refs/cached
-    rm -rf "$worktree"
-    git worktree prune
-    git fetch --prune origin
-    git fetch origin cached:cached || true
-    git worktree add "$worktree" --orphan || git worktree add "$worktree"
-    cd "$worktree"
+  rm -rf "$worktree"
+  git worktree prune
+  git fetch --prune origin
+  git fetch origin cached:cached || true
+  git worktree add "$worktree" --orphan || git worktree add "$worktree"
+  cd "$worktree"
 
-    "$0" generate . "$root/$argc_paths" --tag "$argc_tag"
+  "$0" generate . "$paths" --tag "$argc_tag"
 
-    git add --all
-    commit() {
-      git -c user.name="$argc_name" -c user.email="$argc_email" commit --message cached-refs "$@"
-    }
-    commit --amend || commit
-    git push --force-with-lease --set-upstream origin cached
-    cd "$root"
-    git worktree remove "$worktree"
+  git add --all
+  commit() {
+    git -c user.name="$argc_name" -c user.email="$argc_email" commit --message cached-refs "$@"
   }
-
-  try_push
+  commit --amend || commit
+  git push --force-with-lease --set-upstream origin cached
+  cd "$root"
+  git worktree remove "$worktree"
 }
 
 # @cmd Wraps nix to use cached if it exists
