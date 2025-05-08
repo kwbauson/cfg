0d79b011e2c884cbc481aac4867a34e57dd60096
create-cached-refs <noreply@example.com>
2025-05-08 11:02:31 -0400

    cached-refs

---
+++ paths/aarch64-darwin.nix
@@ -0,0 +1,6 @@
+{ mkPath }:
+{
+  __sourceHash = "p7c5r9mj8rd37r7bhrc586lf56nr084h";
+  "switch"."scripts"."readlee-mac-m1"."nds" = mkPath /nix/store/qww4znjs73f4428h3ljgfy65ryjqzq9g-switch;
+  "switch"."scripts"."readlee-mac-m1"."noa" = mkPath /nix/store/s6p1w7w6n5r71vhn8kk31yy4cmaxhw28-readlee-mac-m1-noa;
+}
---
+++ paths/x86_64-linux.nix
@@ -0,0 +1,16 @@
+{ mkPath }:
+{
+  __sourceHash = "p7c5r9mj8rd37r7bhrc586lf56nr084h";
+  "switch"."scripts"."keith-desktop"."noa" = mkPath /nix/store/p19p8g2zwi056b2p3h590w7m3n3p21x5-keith-desktop-noa;
+  "switch"."scripts"."keith-desktop"."nob" = mkPath /nix/store/z37i52aflkyiaqw2db8qhhcx76ca2hzp-switch;
+  "switch"."scripts"."keith-desktop"."nos" = mkPath /nix/store/n3mdv5vi4k59bmkwrigf3flvfccmpkwy-switch;
+  "switch"."scripts"."keith-server"."noa" = mkPath /nix/store/jc66h0i7sq2dxzgs8d4g5rzg1iz2j942-keith-server-noa;
+  "switch"."scripts"."keith-server"."nob" = mkPath /nix/store/g7gm3cz1s6vqad3s0mip429wfyiyncmr-switch;
+  "switch"."scripts"."keith-server"."nos" = mkPath /nix/store/qjxhksd8mb4rskbaga3m9yvpwdl4yz4d-switch;
+  "switch"."scripts"."keith-xps"."noa" = mkPath /nix/store/lmkr31j401bb3yrnimw3gyj4djx7lriw-keith-xps-noa;
+  "switch"."scripts"."keith-xps"."nob" = mkPath /nix/store/g2z9487n7hlccgc0p5asrm67sqgzih21-switch;
+  "switch"."scripts"."keith-xps"."nos" = mkPath /nix/store/6chf5y5adpadclgv2zv4m4mjvg4nzrqa-switch;
+  "switch"."scripts"."kwbauson"."noa" = mkPath /nix/store/gda6q36q3yma2mzx14dawnzz8amr1nk9-kwbauson-noa;
+  "switch"."scripts"."kwbauson"."nob" = mkPath /nix/store/2qj1jisia93ywph4jn3zj26nbva2jppw-switch;
+  "switch"."scripts"."kwbauson"."nos" = mkPath /nix/store/9i5avms4z4l72kav1i9qxmlds8vyjb16-switch;
+}
