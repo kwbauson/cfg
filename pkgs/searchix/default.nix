76367476e61781e5dd7035318d5327ceee8f4ece
Keith Bauson <kwbauson@gmail.com>
2025-05-10 10:57:23 -0400

    searchix: tweak search rules a bit

--- pkgs/searchix/default.nix
+++ pkgs/searchix/default.nix
@@ -2,10 +2,13 @@ scope: with scope;
 importPackage (attrs: {
   inherit pname;
   version = "0.1.38-unstable-2025-05-09";
-  src = fetchgit {
-    url = "https://codeberg.org/alanpearce/searchix.git";
-    rev = "dae1f9319556516fb10ef6bfe2058690639c1891";
-    hash = "sha256-D0qkde1cyGWD7nzsYIzyp/SmERd8BAF1sCs+Elf8cr0=";
+  src = applyPatches {
+    patches = [ ./search-tweaks.patch ];
+    src = fetchgit {
+      url = "https://codeberg.org/alanpearce/searchix.git";
+      rev = "dae1f9319556516fb10ef6bfe2058690639c1891";
+      hash = "sha256-D0qkde1cyGWD7nzsYIzyp/SmERd8BAF1sCs+Elf8cr0=";
+    };
   };
   passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
 
