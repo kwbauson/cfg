0924fcc193fd3a9a3f50a54e974058af9e5d858f
Keith Bauson <kwbauson@gmail.com>
2025-05-16 12:31:05 -0400

    Revert overriding vendorHash in zaddy

--- pkgs/zaddy.nix
+++ pkgs/zaddy.nix
@@ -1,7 +1,6 @@
 scope: with scope;
 cobi.pkgs.zaddy.overrideAttrs {
   postInstall = caddy.postInstall;
-  vendorHash = "sha256-EUJqeo6sWT5N334MQ73iNVcOMVEalu31vescu9ckm5g=";
   passthru = { };
   meta.skipBuild = isDarwin;
 }
