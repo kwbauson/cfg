970d4e6fbd8403662c739198ccecfcaf2d062b8a
Keith Bauson <kwbauson@gmail.com>
2025-06-09 03:24:59 -0400

    all: update (#2215)

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2025-06-08";
+  version = "0-unstable-2025-06-09";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "d90f00f297fa4d02253780901da304c2dbf71966";
-    hash = "sha256-OXXLfzQ2om/r9jLemr/DZrWhWcEc5a4tugD/LGHnIm0=";
+    rev = "9c6bd106ec068a2eafde5b9e8184c96a4f8d0ffc";
+    hash = "sha256-pE/ELWQhBsVtHbfVhahmZMWesR1HqQ1uOyeYFjXVPU4=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
--- pkgs/nixvim.nix
+++ pkgs/nixvim.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage rec {
   inherit pname;
-  version = "0-unstable-2025-06-06";
+  version = "0-unstable-2025-06-08";
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = pname;
-    rev = "00524c7935f05606fd1b09e8700e9abcc4af7be8";
-    hash = "sha256-In+NjXI8kfJpamTmtytt+rnBzQ213Y9KW55IXvAAK/4=";
+    rev = "2b6f694b48f43bbd89dcc21e8aa7aa676eb18eb8";
+    hash = "sha256-QiB3xDyHuj2VzS6AaALTeikLt6EsZyMjDRmzb4y2vFM=";
   };
   passthru.updateScript = unstableGitUpdater { };
   flake = import src;
