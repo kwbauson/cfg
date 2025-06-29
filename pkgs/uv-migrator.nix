2f264e3e6dc1c730e598508bd4aced88469ba2b7
Keith Bauson <kwbauson@gmail.com>
2025-06-29 03:26:50 -0400

    all: update (#2221)

--- pkgs/uv-migrator.nix
+++ pkgs/uv-migrator.nix
@@ -1,15 +1,15 @@
 scope: with scope;
 rustPlatform.buildRustPackage {
   inherit pname;
-  version = "2025.8.1-unstable-2025-06-06";
+  version = "2025.8.2-unstable-2025-06-20";
   src = fetchFromGitHub {
     owner = "stvnksslr";
     repo = pname;
-    rev = "b8da54f4554810949439e21573813f0545f75d85";
-    hash = "sha256-SzO9Z2y7LQ+AE90pGN734Z+0FOa3mcDeI7KMKqoWW4o=";
+    rev = "ccb2afd304d6bd8864b5c28f890c016b39dbe3bd";
+    hash = "sha256-ZrHMCfkNz+Ufae1Ylze4poPtYSb8PvW56juR3iLYIOk=";
   };
   useFetchCargoVendor = true;
-  cargoHash = "sha256-DsK8hDftdkVxxWuxCa8iHCDsD7huQAomj2fMSA7LjOY=";
+  cargoHash = "sha256-0t18fKYOp6qF+V24tPLK9IUUYp8OTghlT4j3qxqc9kw=";
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/uv-migrator --prefix PATH : ${makeBinPath [ uv_050 ]}
