a874b9bba46cf545e0522b3967191a3645ff48e9
Keith Bauson <kwbauson@gmail.com>
2025-09-02 21:23:45 -0400

    Revert "manual update cobi"
    This reverts commit dc08f8312a3db390569a58c4b37e4b730118f457.

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2025-09-03";
+  version = "0-unstable-2025-08-21";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "d09fa0d3d5ec7e65a893e7b1eda700bf5e24f6b8";
-    hash = "sha256-MAftXo4dtiYwTrADHw/8yVhZI7w8ATJ/Q5RyCjU1zG8=";
+    rev = "63e5892f82504db10c773e2a3e1af0e830a24321";
+    hash = "sha256-sNJRHKHXZUh3mVILZab3ApCwT0bl09O6UkStxsMjo0s=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
