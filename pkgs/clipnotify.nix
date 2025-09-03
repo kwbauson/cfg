fa8d11d709bbbb6e634bfe52a7d435215a080cb2
Keith Bauson <kwbauson@gmail.com>
2025-09-03 19:20:39 -0400

    clipnotify: pause updates check

--- pkgs/clipnotify.nix
+++ pkgs/clipnotify.nix
@@ -7,5 +7,5 @@ prev.overrideAttrs (_: {
     rev = "25ba143c7da8ae0f196cb0db2797d30e6d04e15c";
     hash = "sha256-m0Ji48cRp4mvhHeNKhXTT4UDK32OUYoMoss/2yc7TDg=";
   };
-  passthru.updateScript = unstableGitUpdater { };
+  # passthru.updateScript = unstableGitUpdater { };
 })
