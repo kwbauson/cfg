4798b2cba2f65d19b85e1ecd57570d9e8a3ffd9e
Keith Bauson <kwbauson@gmail.com>
2025-12-02 20:18:13 -0500

    manual updates because zaddy eval

--- pkgs/cobi.nix
+++ pkgs/cobi.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "0-unstable-2025-11-24";
+  version = "0-unstable-2025-12-03";
   src = fetchFromGitHub {
     owner = "jpetrucciani";
     repo = "nix";
-    rev = "d4ee9f0ea9a361a456a0c45ac2b6de19bdf18d70";
-    hash = "sha256-XqwrT8wBZW2pgZBUJB88fmWIkHobV1oryOCiUoaXg9Y=";
+    rev = "d5642e2caccf36668e958ce245a1127fade1a04f";
+    hash = "sha256-lwZ6KYYV5MbzQxkUioYLik8TEmQcn0zMgPjqhnQbmsU=";
   };
   pkgs = (import attrs.src { inherit nixpkgs system; });
   passthru.updateScript = unstableGitUpdater { };
