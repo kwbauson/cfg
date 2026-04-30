16ec25e861f6e594ca5ac7c81c7e284ab84e3664
Keith Bauson <kwbauson@gmail.com>
2026-04-30 11:07:24 -0400

    try this

--- default.nix
+++ default.nix
@@ -4,6 +4,12 @@ let
   lock = fromJSON (readFile ./flake.lock);
   flake-compat = with lock.nodes.flake-compat.locked;
     import (fetchTarball { inherit url; sha256 = narHash; });
-  getFlakeCompat = src: (flake-compat { inherit src; useBuiltinsFetchTree = true; }).defaultNix;
+  getFlakeCompat = src: (flake-compat {
+    # do own copying to store since lix flake-compat doesn't fetchGit shallow clones
+    # see https://github.com/NixOS/flake-compat/pull/75
+    src = if builtins ? fetchGit && pathExists (src + "/git") then fetchGit src else src;
+    copySourceTreeToStore = false;
+    useBuiltinsFetchTree = true;
+  }).defaultNix;
 in
 (getFlakeCompat ./.).packages.${system}.scope // { inherit getFlakeCompat; }
