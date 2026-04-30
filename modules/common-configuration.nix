f2630ef0e6e33825f513f848d3d28e8b0d3458be
Keith Bauson <kwbauson@gmail.com>
2026-04-30 14:15:38 -0400

    sigh

--- modules/common-configuration.nix
+++ modules/common-configuration.nix
@@ -15,6 +15,7 @@
     fallback = true;
     trusted-users = [ "@wheel" ];
     extra-experimental-features = [ "nix-command" "flakes" ];
+    extra-deprecated-features = [ "shadow-internal-symbols" "broken-string-escape" ];
     narinfo-cache-negative-ttl = 10;
     extra-substituters = [ "https://kwbauson.cachix.org" ];
     extra-trusted-public-keys = [ "kwbauson.cachix.org-1:a6RuFyeJKSShV8LAUw3Jx8z48luiCU755DkweAAkwX0=" ];
--- pkgs/clipnotify.nix
+++ pkgs/clipnotify.nix
@@ -1,11 +1,10 @@
 scope: with scope;
-prev.overrideAttrs (_: {
+prev.overrideAttrs (attrs: {
   version = "1.0.2-unstable-2022-11-08";
   src = fetchFromGitHub {
-    owner = "cdown";
-    repo = pname;
+    inherit (attrs.src) owner repo;
     rev = "25ba143c7da8ae0f196cb0db2797d30e6d04e15c";
     hash = "sha256-m0Ji48cRp4mvhHeNKhXTT4UDK32OUYoMoss/2yc7TDg=";
   };
-  # passthru.updateScript = unstableGitUpdater { };
+  # passthru.updateScript = unstableGitUpdater { branch = "master"; };
 })
