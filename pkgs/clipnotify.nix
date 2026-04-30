ed8ecad65f1c8ec251581a16dd1fba5fe19c5279
Keith Bauson <kwbauson@gmail.com>
2026-04-30 14:22:16 -0400

    undo this stuff

--- pkgs/clipnotify.nix
+++ pkgs/clipnotify.nix
@@ -1,10 +1,11 @@
 scope: with scope;
-prev.overrideAttrs (attrs: {
+prev.overrideAttrs (_: {
   version = "1.0.2-unstable-2022-11-08";
   src = fetchFromGitHub {
-    inherit (attrs.src) owner repo;
+    owner = "cdown";
+    repo = pname;
     rev = "25ba143c7da8ae0f196cb0db2797d30e6d04e15c";
     hash = "sha256-m0Ji48cRp4mvhHeNKhXTT4UDK32OUYoMoss/2yc7TDg=";
   };
-  # passthru.updateScript = unstableGitUpdater { branch = "master"; };
+  # passthru.updateScript = unstableGitUpdater { };
 })
