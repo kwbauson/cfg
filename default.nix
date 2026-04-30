1038e5733de9776b0d6ab74479b95a5a00d16a05
Keith Bauson <kwbauson@gmail.com>
2026-04-30 09:35:08 -0400

    try using fetchtree in flake-compat

--- default.nix
+++ default.nix
@@ -4,6 +4,6 @@ let
   lock = fromJSON (readFile ./flake.lock);
   flake-compat = with lock.nodes.flake-compat.locked;
     import (fetchTarball { inherit url; sha256 = narHash; });
-  getFlakeCompat = src: (flake-compat { inherit src; }).defaultNix;
+  getFlakeCompat = src: (flake-compat { inherit src; useBuiltinsFetchTree = true; }).defaultNix;
 in
 (getFlakeCompat ./.).packages.${system}.scope // { inherit getFlakeCompat; }
