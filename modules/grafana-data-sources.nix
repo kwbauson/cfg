f88f902a33227caf044f79c7e1ec1f9d2ea2f74e
Keith Bauson <kwbauson@gmail.com>
2026-05-24 16:50:04 -0400

    modules/grafana-datasources: use mkMerge instead of import

--- modules/grafana-data-sources.nix
+++ modules/grafana-data-sources.nix
@@ -1,30 +1,31 @@
-{ scope, ... }: with scope; {
-  services.prometheus.exporters.node = {
-    enable = true;
-    enabledCollectors = optionals isLinux [ "systemd" "processes" ];
-    port = constants.prometheus.exporters.node.port;
-    listenAddress = machine.tailscale-ip;
-  };
-  imports = [
-    (optionalAttrs isLinux {
-      systemd.services.prometheus-node-exporter.after = [ "tailscaled.service" ];
+{ scope, ... }: with scope;
+mkMerge [
+  {
+    services.prometheus.exporters.node = {
+      enable = true;
+      enabledCollectors = optionals isLinux [ "systemd" "processes" ];
+      port = constants.prometheus.exporters.node.port;
+      listenAddress = machine.tailscale-ip;
+    };
+  }
+  (mkIf isLinux {
+    systemd.services.prometheus-node-exporter.after = [ "tailscaled.service" ];
 
-      services.alloy.enable = true;
-      systemd.services.alloy.serviceConfig.SupplementaryGroups = [ "adm" "systemd-journal" ];
-      environment.etc."alloy/config.alloy".text = ''
-        loki.source.journal "systemd_journal" {
-          format_as_json = true
-          forward_to = [loki.write.grafana_loki.receiver]
-          labels = {
-            instance = "${machine.name}",
-          }
+    services.alloy.enable = true;
+    systemd.services.alloy.serviceConfig.SupplementaryGroups = [ "adm" "systemd-journal" ];
+    environment.etc."alloy/config.alloy".text = ''
+      loki.source.journal "systemd_journal" {
+        format_as_json = true
+        forward_to = [loki.write.grafana_loki.receiver]
+        labels = {
+          instance = "${machine.name}",
         }
-        loki.write "grafana_loki" {
-          endpoint {
-            url = "http://keith-server:${toString constants.loki.port}/loki/api/v1/push"
-          }
+      }
+      loki.write "grafana_loki" {
+        endpoint {
+          url = "http://keith-server:${toString constants.loki.port}/loki/api/v1/push"
         }
-      '';
-    })
-  ];
-}
+      }
+    '';
+  })
+]
