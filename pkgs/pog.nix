be2fb755d91d24f8373f253279c0c0303b44674b
Keith Bauson <kwbauson@gmail.com>
2026-06-07 11:10:27 -0400

    stop pog from using a new nixpkgs

--- pkgs/pog.nix
+++ pkgs/pog.nix
@@ -1,2 +1,10 @@
 scope: with scope;
-cobi.pkgs.pog
+let
+  lock = fromJSON (readFile "${cobi.src}/flake.lock");
+  locked = lock.nodes.${lock.nodes.root.inputs.pog}.locked;
+  src = fetchTarball {
+    url = "https://github.com/${locked.owner}/${locked.repo}/archive/${locked.rev}.tar.gz";
+    sha256 = locked.narHash;
+  };
+in
+(import "${src}/pog" { inherit pkgs system; }).pog
