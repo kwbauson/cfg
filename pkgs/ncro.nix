af2500f31281ee5a4a9c26904a3b6b44bc106dc8
Keith Bauson <kwbauson@gmail.com>
2026-06-08 12:59:50 -0400

    ncro: update repo owner

--- pkgs/ncro.nix
+++ pkgs/ncro.nix
@@ -3,7 +3,7 @@ importPackage (attrs: {
   inherit pname;
   version = "2.2.2-unstable-2026-06-05";
   src = fetchFromGitHub {
-    owner = "feel-co";
+    owner = "manic-systems";
     repo = pname;
     rev = "acd3855d7e0d9bc298a0019da46452c664f24081";
     hash = "sha256-attdCg/FjUooYxVidEDR5wVeQ8aAPAj4b6HQVL17Tng=";
