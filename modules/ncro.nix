e6ccbace40cf0b70e5057037db04e33ab2ae0932
Keith Bauson <kwbauson@gmail.com>
2026-06-08 22:20:15 -0400

    bring back ncro priorities

--- modules/ncro.nix
+++ modules/ncro.nix
@@ -6,18 +6,26 @@
   services.ncro = {
     enable = true;
     settings.server.listen = "localhost:9180";
-    settings.upstreams = [
+    settings.upstreams = imap (i: a: { priority = i; } // a) [
+      {
+        url = "http://${machines.keith-server.tailscale.fqdn}:5000";
+        public_key = "${machines.keith-server.tailscale.fqdn}:amORAvA0d0VWxUnZyLPJXEY7QEKebU4SqURpe1CbsDY=";
+      }
+      {
+        url = "http://${machines.readlee-mac-m1.tailscale.fqdn}:5000";
+        public_key = "${machines.readlee-mac-m1.tailscale.fqdn}:dHyGth5OeFh3Tg2OFAQHFwtvOLJbOB/tYEzJIuogWcc=";
+      }
       {
         url = "https://cache.nixos.org";
         public_key = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
       }
       {
-        url = "https://kwbauson.cachix.org";
-        public_key = "kwbauson.cachix.org-1:a6RuFyeJKSShV8LAUw3Jx8z48luiCU755DkweAAkwX0=";
-      }
-      {
         url = "https://nix-community.cachix.org";
         public_key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      }
+      {
+        url = "https://kwbauson.cachix.org";
+        public_key = "kwbauson.cachix.org-1:a6RuFyeJKSShV8LAUw3Jx8z48luiCU755DkweAAkwX0=";
       }
       {
         url = "https://benaduggan.cachix.org";
@@ -30,14 +38,6 @@
       {
         url = "https://devenv.cachix.org";
         public_key = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-      }
-      {
-        url = "http://${machines.keith-server.tailscale.fqdn}:5000";
-        public_key = "${machines.keith-server.tailscale.fqdn}:amORAvA0d0VWxUnZyLPJXEY7QEKebU4SqURpe1CbsDY=";
-      }
-      {
-        url = "http://${machines.readlee-mac-m1.tailscale.fqdn}:5000";
-        public_key = "${machines.readlee-mac-m1.tailscale.fqdn}:dHyGth5OeFh3Tg2OFAQHFwtvOLJbOB/tYEzJIuogWcc=";
       }
     ];
   };
