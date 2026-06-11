75fd266919c25301efbe474f4e3b695cb1432dbf
Keith Bauson <kwbauson@gmail.com>
2026-06-11 17:20:03 -0400

    optionsdoc: better flake stuff

--- pkgs/optionsdoc.nix
+++ pkgs/optionsdoc.nix
@@ -2,12 +2,7 @@ scope: with scope;
 (writeBashBin "optionsdoc" ''
   set -euo pipefail
   pathStr=''${1-${if isLinux then "nixos" else "nix-darwin"}}
-  MANPATH=$(
-    nix build --no-link --print-out-paths \
-      "${flake}#optionsdoc.builds.$pathStr"
-  )
-  export MANPATH
-  ${getExe man} options
+  exec nix shell "${flake}#optionsdoc.run.$pathStr" -c options-man
 '').overrideAttrs (finalAttrs: previousAttrs:
 let inherit (finalAttrs) passthru; in {
   meta = previousAttrs.meta // { includePackage = true; };
@@ -32,24 +27,34 @@ let inherit (finalAttrs) passthru; in {
           'self._options_by_id.get(links[i], links[i].lstrip("#opt-"))'
       '';
     };
-    result = runCommandLocal "options-man" { } ''
-      mkdir -p $out/man5
-      file=$out/man5/options.5
-      ${getExe nixos-render-docs} -j $NIX_BUILD_CORES options manpage \
-        --revision none \
-        ${optionsJSON}/${common.outputPath}/options.json \
-        $file
-      headerStart='\.SH "NAME"'
-      headerEnd='^You can use the following options in'
-      sed -i \
-        -e '1s/"CONFIGURATION.*/"OPTIONS" "5"/' \
-        -e "0,/$headerEnd/{/$headerStart/,/$headerEnd/d}" \
-        -e '/\.SH "AUTHORS"/,/^Eelco/d' \
-        $file
-    '';
+    result = runCommandLocal "options-man"
+      {
+        passAsFile = [ "script" ];
+        script = ''
+          #!/bin/sh
+          MANPATH=${placeholder "out"} man options
+        '';
+      }
+      ''
+        mkdir -p $out/{bin,man5}
+        file=$out/man5/options.5
+        ${getExe nixos-render-docs} -j $NIX_BUILD_CORES options manpage \
+          --revision none \
+          ${optionsJSON}/${common.outputPath}/options.json \
+          $file
+        headerStart='\.SH "NAME"'
+        headerEnd='^You can use the following options in'
+        sed -i \
+          -e '1s/"CONFIGURATION.*/"OPTIONS" "5"/' \
+          -e "0,/$headerEnd/{/$headerStart/,/$headerEnd/d}" \
+          -e '/\.SH "AUTHORS"/,/^Eelco/d' \
+          $file
+        cp $scriptPath $out/bin/options-man
+        chmod +x $out/bin/options-man
+      '';
   }.result;
   passthru = {
-    builds =
+    run =
       let
         mapper = path: f: mapAttrs (name: value: let p = path ++ [ name ]; in f p name value (mapper p f value));
       in
@@ -57,7 +62,7 @@ let inherit (finalAttrs) passthru; in {
         (
           path: _: value: rest:
             let built = passthru.build { inherit path; }; in
-            (if isOption value then { } else rest) // { inherit (built) outPath drvPath; type = "derivation"; }
+            (if isOption value then { } else rest) // { type = "derivation"; inherit (built) outPath drvPath; }
         )
         passthru.allOptions;
     baseOptions = fix (self: {
