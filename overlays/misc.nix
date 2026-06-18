64dd2e711a499436c172b8ab941f23d79ad7f7ea
Keith Bauson <kwbauson@gmail.com>
2026-06-18 17:04:11 -0400

    hello

--- overlays/misc.nix
+++ overlays/misc.nix
@@ -2,7 +2,12 @@ final: prev: with final.scope; {
   pkgsUnsupported = importNixpkgs { config = root.config // { allowUnsupportedSystem = true; }; };
   pkgsInsecure = importNixpkgs { config = root.config // { allowInsecurePredicate = _: true; }; };
   inherit (extra-bin-packages) fordir;
-  configdiffNix = cfg.outPath;
+  cfgWithoutSubmodules = runCommandLocal "cfg-modules" { } ''
+    cp -r ${cfg.outPath} $out
+    chmod -R +w $out
+    sed -i '/^\s*self.submodules = true;$/d' $out/flake.nix
+  '';
+  configdiffNix = cfgWithoutSubmodules.outPath;
   configdiffNixAttr = "configdiff";
   iso = with packages.x86_64-linux; (nixos ({ modulesPath, ... }: {
     imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix" ];
