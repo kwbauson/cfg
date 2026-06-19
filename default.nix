d957c8ff1da13b518cf42f78b9739251eb545056
Keith Bauson <kwbauson@gmail.com>
2026-06-19 12:14:06 -0400

    fix for flake-compat not handling root flake submodules

--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
 with builtins;
 { system ? currentSystem }:
 let
+  fetchFlakeGit = src: fetchGit {
+    url = src;
+    # flake-compat doesn't handle root flakes with submodules
+    submodules = (import (src + "/flake.nix")).inputs.self.submodules or false;
+  };
+  getSrc = src: if pathExists (src + "/.git") then fetchFlakeGit src else src;
   lock = fromJSON (readFile ./flake.lock);
   flake-compat = with lock.nodes.flake-compat.locked;
     import (fetchTarball { inherit url; sha256 = narHash; });
-  getFlakeCompat = src: (flake-compat { inherit src; }).defaultNix;
+  getFlakeCompat = src: (flake-compat { src = getSrc src; }).defaultNix;
 in
 (getFlakeCompat ./.).packages.${system}.scope // { inherit getFlakeCompat; }
