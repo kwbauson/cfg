1b8500f955fac2ee12fc12ed190f80fa3089bc00
Keith Bauson <kwbauson@gmail.com>
2026-06-19 12:20:50 -0400

    add note about flake-compat pr to support root submodules

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ let
   fetchFlakeGit = src: fetchGit {
     url = src;
     # flake-compat doesn't handle root flakes with submodules
+    # remove this logic when https://github.com/NixOS/flake-compat/pull/93 gets merged
     submodules = (import (src + "/flake.nix")).inputs.self.submodules or false;
   };
   getSrc = src: if pathExists (src + "/.git") then fetchFlakeGit src else src;
