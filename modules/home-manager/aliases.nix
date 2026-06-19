363f5ab6b09d3f5b7a99d265ae7c36f2228769d5
Keith Bauson <kwbauson@gmail.com>
2026-06-19 11:02:57 -0400

    omega hack to mark cfg as dirty when there are changes in submodules

--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -1,4 +1,49 @@
 { scope, ... }: with scope;
+let
+  nixPkgAlias = cmd: extra: /* bash */ ''
+    set -euo pipefail
+    pkg=$1
+    shift
+
+    cd ~/cfg
+    changes=()
+    while read _ path;do
+      changes+=("$path")
+    done < <(git status --short)
+    submodules=()
+    while read _ path _;do
+      submodules+=("$path")
+    done < <(git submodule status)
+
+    markerName=.__submodule_change_marker__
+
+    cleanup() {
+      cd ~/cfg
+      rm -f "$markerName"
+      git rm --force --quiet "$markerName"
+    }
+
+    submoduleChanged=0
+    for change in "''${changes[@]}";do
+      for submodule in "''${submodules[@]}";do
+        if [[ $change = $submodule ]];then
+          submoduleChanged=1
+          break
+        fi
+      done
+      if [[ $submoduleChanged = 1 ]];then
+        touch "$markerName"
+        trap cleanup EXIT
+        break
+      fi
+    done
+
+    git add --all --intent-to-add
+    cd "$OLDPWD"
+    installableArgs=$(echo "$pkg" | sed -E "s@^|,@ $HOME/cfg#@g")
+    nix ${cmd} $installableArgs ${extra} "$@"
+  '';
+in
 {
   included-packages = alias {
     undup = ''tac "$@" | awk '!x[$0]++' | tac'';
@@ -13,10 +58,11 @@
     nou = "cfgu && noa";
     nod = ''delete-old-generations "$@" && nix store gc -v ${optionalString isNixOS "&& sudo /nix/var/nix/profiles/system/bin/switch-to-configuration boot"}'';
     noc = "cd ~/cfg && gh workflow run updates.yml";
-    # TODO make these less surprising with submodules
-    nb = ''pkg=$1 && shift; git -C ~/cfg add --all -N && nix build $(echo "$pkg" | sed -E "s@^|,@ $HOME/cfg#@g")'';
-    ns = ''pkg=$1 && shift; git -C ~/cfg add --all -N && nix shell $(echo "$pkg" | sed -E "s@^|,@ $HOME/cfg#@g")'';
-    nr = ''pkg=$1 && shift; git -C ~/cfg add --all -N && nix run $(echo "$pkg" | sed -E "s@^|,@ $HOME/cfg#@g") --'';
+    nb = nixPkgAlias "build" "";
+    ns = nixPkgAlias "shell" "";
+    nr = nixPkgAlias "run" "--";
+    batwhich = ''bat "$(which "$@")"'';
+    realwhich = ''realpath "$(which "$@")"'';
     reboot-windows = "systemctl reboot --boot-loader-entry=auto-windows";
     lr = ''find "$@" -print0 | sort -z | xargs -0 ls --color=auto -lhd'';
     ${attrIf (isLinux && isGraphical) "nm-applet"} = getExe networkmanagerapplet;
