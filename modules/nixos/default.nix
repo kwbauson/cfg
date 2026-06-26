a7e30639acc9f03cb7787799751f12d1bb55b3f6
Keith Bauson <kwbauson@gmail.com>
2026-06-26 09:22:24 -0400

    all the manually set ssh algos are included by default now

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -120,22 +120,6 @@
     PasswordAuthentication = mkDefault false;
     PermitRootLogin = "no";
     X11Forwarding = true;
-    KexAlgorithms = [
-      "mlkem768x25519-sha256"
-      "sntrup761x25519-sha512"
-      "curve25519-sha256"
-      "curve25519-sha256@libssh.org"
-    ];
-    Ciphers = [
-      "chacha20-poly1305@openssh.com"
-      "aes256-gcm@openssh.com"
-      "aes256-ctr"
-    ];
-    Macs = [
-      "hmac-sha2-512-etm@openssh.com"
-      "hmac-sha2-256-etm@openssh.com"
-      "umac-128-etm@openssh.com"
-    ];
   };
 
   services.caddy.email = "kwbauson@gmail.com";
