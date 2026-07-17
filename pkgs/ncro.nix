bd9f48f8b267af1afc58c469df9de82406db040f
Keith Bauson <kwbauson@gmail.com>
2026-07-17 06:19:10 -0400

    all: update (#2534)

--- pkgs/ncro.nix
+++ pkgs/ncro.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage (attrs: {
   inherit pname;
-  version = "2.2.2-unstable-2026-07-15";
+  version = "2.2.2-unstable-2026-07-16";
   src = fetchFromGitHub {
     owner = "manic-systems";
     repo = pname;
-    rev = "e3bb5315173a41d08e16398dfea76a8e8643da90";
-    hash = "sha256-ZLBvlks0WfeYQPEMoJqqMDFsBAsFa9dol75SBnURqco=";
+    rev = "8681ec50b9570de825786f9ec599b1301214a1ff";
+    hash = "sha256-E4sJw/ySR3bEGd3eyqXErmDtjj0TF+8JGYXH9uDPctc=";
   };
   package = callPackage "${attrs.src}/nix/package.nix" { };
   passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
