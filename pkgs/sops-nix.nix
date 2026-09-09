8f736d813a0291f9fb7e3ccae47955c1ab39fa62
Keith Bauson <kwbauson@gmail.com>
2026-09-09 07:03:22 -0400

    all: update

--- pkgs/sops-nix.nix
+++ pkgs/sops-nix.nix
@@ -1,12 +1,12 @@
 scope: with scope;
 importPackage {
   inherit pname;
-  version = "0-unstable-2026-09-02";
+  version = "0-unstable-2026-09-09";
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = pname;
-    rev = "fbf759290e0cb0a98dfc813a4eb7d53ad1dacb57";
-    hash = "sha256-gkSH8VUtCo6hnysNmb9DbTuDepH2t5pv+QWjP75xKAk=";
+    rev = "13616fff713a9f94055c66f15687ebdc17a335df";
+    hash = "sha256-4GuMPW90JSxXWDPUB9M+1m7fYbe3H0apOd86/zBQ2Kw=";
   };
   passthru.updateScript = unstableGitUpdater { hardcodeZeroVersion = true; };
 }
